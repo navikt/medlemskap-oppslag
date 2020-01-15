9d78d4de6bef7818eac00ac71f1e9ce73181d03d
Hågen Hasle <haagenhasle@gmail.com>
2020-01-15 13:03:24 +0100

    Tatt med json-serialiseringen av javatime også på klienten, hadde glemt det der..

--- src/main/kotlin/no/nav/medlemskap/common/HttpClient.kt
+++ src/main/kotlin/no/nav/medlemskap/common/HttpClient.kt
@@ -4,11 +4,11 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
-import no.nav.medlemskap.localDateDeserializer
-import no.nav.medlemskap.localDateSerializer
+import no.nav.medlemskap.*
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.net.ProxySelector
 import java.time.LocalDate
+import java.time.LocalDateTime
 
 internal val defaultHttpClient = HttpClient(Apache) {
     install(JsonFeature) {
@@ -16,6 +16,10 @@ internal val defaultHttpClient = HttpClient(Apache) {
             disableHtmlEscaping()
             registerTypeAdapter(LocalDate::class.java, localDateDeserializer)
             registerTypeAdapter(LocalDate::class.java, localDateSerializer)
+            registerTypeAdapter(LocalDateTime::class.java, localDateTimeDeserializer)
+            registerTypeAdapter(LocalDateTime::class.java, localDateTimeSerializer)
+            registerTypeAdapter(LocalDate::class.java, yearMonthDeserializer)
+            registerTypeAdapter(LocalDate::class.java, yearMonthSerializer)
         }
     }
     engine {
