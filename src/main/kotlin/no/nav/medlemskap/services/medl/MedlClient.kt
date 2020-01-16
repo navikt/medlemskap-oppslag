cb3fc5aefd9b8b631064b6dbdac9e68fba1fa8f4
Richard Borge <richard.borge@nav.no>
2020-01-16 09:44:30 +0100

    Bruker riktig token

--- src/main/kotlin/no/nav/medlemskap/services/medl/MedlClient.kt
+++ src/main/kotlin/no/nav/medlemskap/services/medl/MedlClient.kt
@@ -12,20 +12,24 @@ import no.nav.medlemskap.services.sts.StsRestClient
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-class MedlClient(val baseUrl: String, val stsClient: StsRestClient, val callIdGenerator: () -> String) {
+class MedlClient(
+        private val baseUrl: String,
+        private val stsClient: StsRestClient,
+        private val callIdGenerator: () -> String
+) {
 
     suspend fun hentMedlemskapsunntak(ident: String, fraOgMed: LocalDate? = null, tilOgMed: LocalDate? = null): List<Medlemskapsunntak> {
-        with(stsClient.oidcToken()) {
-            return defaultHttpClient.get<List<Medlemskapsunntak>> {
-                url("$baseUrl/api/v1/medlemskapsunntak")
-                header(HttpHeaders.Authorization, "Bearer ${this}")
-                header(HttpHeaders.Accept, ContentType.Application.Json)
-                header("Nav-Call-Id", callIdGenerator.invoke())
-                header("Nav-Personident", ident)
-                fraOgMed?.let { parameter("fraOgMed", fraOgMed.tilIsoFormat()) }
-                tilOgMed?.let { parameter("tilOgMed", tilOgMed.tilIsoFormat()) }
-            }
+        val token = stsClient.oidcToken()
+        return defaultHttpClient.get{
+            url("$baseUrl/api/v1/medlemskapsunntak")
+            header(HttpHeaders.Authorization, "Bearer $token")
+            header(HttpHeaders.Accept, ContentType.Application.Json)
+            header("Nav-Call-Id", callIdGenerator.invoke())
+            header("Nav-Personident", ident)
+            fraOgMed?.let { parameter("fraOgMed", fraOgMed.tilIsoFormat()) }
+            tilOgMed?.let { parameter("tilOgMed", tilOgMed.tilIsoFormat()) }
         }
+
     }
 
     private fun LocalDate.tilIsoFormat() = this.format(DateTimeFormatter.ISO_DATE)
