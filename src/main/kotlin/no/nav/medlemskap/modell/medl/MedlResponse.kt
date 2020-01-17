236a2953bcfbcd5ee1739902db6b84baa1e11df3
Richard Borge <richard.borge@nav.no>
2020-01-17 10:07:57 +0100

    Gjør om til dato på medl

--- src/main/kotlin/no/nav/medlemskap/modell/medl/MedlResponse.kt
+++ src/main/kotlin/no/nav/medlemskap/modell/medl/MedlResponse.kt
@@ -1,8 +1,11 @@
 package no.nav.medlemskap.modell.medl
 
+import java.time.LocalDate
+import java.time.LocalDateTime
+
 data class Medlemskapsunntak (
         val dekning: String?,
-        val fraOgMed: String, // ISO-8601 dato
+        val fraOgMed: LocalDate,
         val grunnlag: String,
         val helsedel: Boolean,
         val ident: String,
@@ -11,19 +14,20 @@ data class Medlemskapsunntak (
         val medlem: Boolean,
         val status: String,
         val statusaarsak: String?,
-        val tilOgMed: String, // ISO-8601 dato
+        val tilOgMed: LocalDate,
         val untakId: Int,
         val sporingsinformasjon: Sporingsinformasjon?,
         val studieinformasjon: Studieinformasjon?
 )
 
 data class Sporingsinformasjon (
-        val besluttet: String, // ISO-8601 dato
+        val besluttet: LocalDate?,
         val kilde: String,
-        val opprettet: String, // ISO-8601 dato-tid
+        val kildedokument: String?,
+        val opprettet: LocalDateTime,
         val opprettetAv: String,
-        val registrert: String, // ISO-8601 dato
-        val sistEndret: String, // ISO-8601 dato-tid
+        val registrert: LocalDate?,
+        val sistEndret: LocalDateTime,
         val sistEndretAv: String,
         val versjon: String
 )
@@ -32,5 +36,6 @@ data class Studieinformasjon (
         val delstudie: Boolean,
         val soeknadInnvilget: Boolean,
         val statsborgerland: String,
-        val studieland: String
+        val studieland: String?
 )
+
