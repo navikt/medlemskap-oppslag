d4595c304114fc4cf3eb5f3e78c99429d2bb68ad
Silje Risvik <silje.risvik@nav.no>
2020-01-17 15:06:52 +0100

    Fikstet feil paa dato

--- src/main/kotlin/no/nav/medlemskap/services/inntekt/InntektClient.kt
+++ src/main/kotlin/no/nav/medlemskap/services/inntekt/InntektClient.kt
@@ -15,27 +15,23 @@ import java.time.format.DateTimeFormatter
 class InntektClient(val baseUrl: String, val stsClient: StsRestClient, val callIdGenerator: () -> String) {
 
     suspend fun hentInntektListe(ident: String, fraOgMed: LocalDate? = null, tilOgMed: LocalDate? = null): List<ArbeidsinntektMaaned> {
-            val token = stsClient.oidcToken()
-            return defaultHttpClient.post<List<ArbeidsinntektMaaned>> {
-                url("$baseUrl/hentinntektliste")
-                header(HttpHeaders.Authorization, "Bearer $token")
-                header(HttpHeaders.Accept, ContentType.Application.Json)
-                header(HttpHeaders.ContentType, ContentType.Application.Json)
-                header("Nav-Consumer-Id", configuration.sts.username)
-                header("Nav-Call-Id", callIdGenerator.invoke())
-                body = HentInntektListeRequest(
-                        Ident(ident, "NATURLIG_IDENT"),
-                        fraOgMed?.tilIsoFormat()?.tilAarOgMnd(),
-                        tilOgMed?.tilIsoFormat()?.tilAarOgMnd())
-            }
+        val token = stsClient.oidcToken()
+        return defaultHttpClient.post<List<ArbeidsinntektMaaned>> {
+            url("$baseUrl/hentinntektliste")
+            header(HttpHeaders.Authorization, "Bearer $token")
+            header(HttpHeaders.Accept, ContentType.Application.Json)
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+            header("Nav-Consumer-Id", configuration.sts.username)
+            header("Nav-Call-Id", callIdGenerator.invoke())
+            body = HentInntektListeRequest(
+                    Ident(ident, "NATURLIG_IDENT"),
+                    fraOgMed?.tilAarOgMnd(),
+                    tilOgMed?.tilAarOgMnd())
+        }
 
     }
 
-    private fun LocalDate.tilIsoFormat() = this.format(DateTimeFormatter.ISO_DATE)
-
-
-
-    private fun String.tilAarOgMnd() =  this.format(DateTimeFormatter.ofPattern("yyyy-mm"))
+    private fun LocalDate.tilAarOgMnd() = this.format(DateTimeFormatter.ofPattern("yyyy-mm"))
 }
 
 
