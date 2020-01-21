8968f78f8c9baafebd954e691c696d89e471921b
Silje Risvik <silje.risvik@nav.no>
2020-01-21 11:07:49 +0100

    Sett paa ignoreUnknown for aa hoppe over felter

--- src/main/kotlin/no/nav/medlemskap/config/AadConfigCall.kt
+++ src/main/kotlin/no/nav/medlemskap/config/AadConfigCall.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import no.nav.medlemskap.common.defaultHttpClient
 
-@JsonIgnoreProperties
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class AzureAdOpenIdConfiguration(
         @JsonProperty("jwks_uri")
         val jwksUri: String,
