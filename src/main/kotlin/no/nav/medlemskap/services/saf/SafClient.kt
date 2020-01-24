1b31d8d1a6424e7dd89ef7882b7251e436529aac
Hågen Hasle <haagenhasle@gmail.com>
2020-01-24 12:25:28 +0100

    Prøver igjen, nå med jackson og serialisering av en simpel dto

--- src/main/kotlin/no/nav/medlemskap/services/saf/SafClient.kt
+++ src/main/kotlin/no/nav/medlemskap/services/saf/SafClient.kt
@@ -7,6 +7,7 @@ import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.content.TextContent
 import no.nav.medlemskap.common.defaultHttpClient
+import no.nav.medlemskap.common.objectMapper
 import no.nav.medlemskap.services.sts.StsRestClient
 
 private const val TEMA_MEDLEMSKAP = "MED"
@@ -16,13 +17,11 @@ private const val ANTALL_JOURNALPOSTER = 10
 
 class SafClient(val baseUrl: String, val stsClient: StsRestClient, val callIdGenerator: () -> String) {
 
-    suspend fun hentJournaldata(fnr: String): SafResponse {
-        val minimalQuery = "{\"query\": \"query{ dokumentoversiktBruker(brukerId: {id: \\\"$fnr\\\", type: FNR}, foerste:5) {journalposter {journalpostId}}}\"}"
-        val query =
-                """
-                { 
-                    "query": "query {
-                      dokumentoversiktBruker(brukerId: {id: \"$fnr\", type: FNR}, tema: [$TEMA_MEDLEMSKAP, $TEMA_UNNTAK_FRA_MEDLEMSKAP, $TEMA_TRYGDEAVGIFT], foerste: $ANTALL_JOURNALPOSTER) {
+    internal open class GraphqlQuery(val query: String, val variables: Any? = null)
+    internal data class DokumentoversiktBrukerQuery(val fnr: String) : GraphqlQuery(
+            query = """ 
+            query {
+                      dokumentoversiktBruker(brukerId: {id: "$fnr", type: FNR}, tema: [$TEMA_MEDLEMSKAP, $TEMA_UNNTAK_FRA_MEDLEMSKAP, $TEMA_TRYGDEAVGIFT], foerste: $ANTALL_JOURNALPOSTER) {
                         journalposter {
                           journalpostId
                           tittel
@@ -36,7 +35,18 @@ class SafClient(val baseUrl: String, val stsClient: StsRestClient, val callIdGen
                           }
                         }
                       }
-                    }"
+                    }
+            """.trimIndent(),
+            variables = null
+    )
+
+    suspend fun hentJournaldata(fnr: String): SafResponse {
+
+        val minimalQuery = "{\"query\": \"query{ dokumentoversiktBruker(brukerId: {id: \\\"$fnr\\\", type: FNR}, foerste:5) {journalposter {journalpostId}}}\"}"
+        val query =
+                """
+                { 
+                    "query": ""
                 }
                 """.trimIndent()
 
@@ -49,7 +59,7 @@ class SafClient(val baseUrl: String, val stsClient: StsRestClient, val callIdGen
             header(HttpHeaders.Accept, ContentType.Application.Json)
             header("Nav-Callid", callIdGenerator.invoke())
             header("Nav-Consumer-Id", "p3-medlemskap")
-            body = TextContent(minimalQuery, contentType = ContentType.Application.Json)
+            body = TextContent(objectMapper.writeValueAsString(DokumentoversiktBrukerQuery(fnr)), contentType = ContentType.Application.Json)
             //header(HttpHeaders.AcceptCharset, Charsets)
         }
     }
