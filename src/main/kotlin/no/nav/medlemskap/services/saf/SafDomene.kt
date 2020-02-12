50b77224bd94b2209b7f010ba1058d65b5a62448
Richard Borge <richard.borge@nav.no>
2020-02-12 10:49:17 +0100

    Fikser mer saf feil

--- src/main/kotlin/no/nav/medlemskap/services/saf/SafDomene.kt
+++ src/main/kotlin/no/nav/medlemskap/services/saf/SafDomene.kt
@@ -8,7 +8,7 @@ data class BrukerIdInput(
         val type: String = "FNR"
 )
 
-enum class JournalStatus {
+enum class Journalstatus {
     MOTTATT,
     JOURNALFOERT,
     FERDIGSTILT,
@@ -89,7 +89,7 @@ enum class Tema(@Suppress("unused") val beskrivelse: String) {
 
 data class DokumentInfo(val dokumentInfoId: String, val tittel: String?)
 
-data class JournalPost(val journalpostId: String, val tittel: String?, val journalposttype: JournalPostType?, val journalstatus: JournalStatus?, val tema: Tema?, val datoOpprettet: LocalDateTime, val dokumenter: List<DokumentInfo>?)
+data class JournalPost(val journalpostId: String, val tittel: String?, val journalposttype: JournalPostType?, val journalstatus: Journalstatus?, val tema: Tema?, val datoOpprettet: LocalDateTime, val dokumenter: List<DokumentInfo>?)
 
 data class Dokumentoversikt(val journalposter: List<JournalPost>)
 
@@ -100,7 +100,7 @@ data class Location(val line: Int, val column: Int)
 @JsonIgnoreProperties(ignoreUnknown=true)
 data class Errors(val message: String, val locations: List<Location>, val path: List<String>?)
 
-data class DokumentoversiktBrukerResponse(val data: Data, val errors: List<Errors>?)
+data class DokumentoversiktBrukerResponse(val data: Data?, val errors: List<Errors>?)
 
 data class GraphqlQuery(val query: String, val variables: Variables)
 
@@ -108,7 +108,7 @@ data class Variables(
         val brukerId: BrukerIdInput,
         val foerste: Int,
         val tema: List<Tema> = listOf(Tema.MED, Tema.UFM, Tema.TRY),
-        val journalstatuser: List<JournalStatus> = listOf(JournalStatus.MOTTATT, JournalStatus.JOURNALFOERT, JournalStatus.FERDIGSTILT, JournalStatus.EKSPEDERT, JournalStatus.UNDER_ARBEID, JournalStatus.RESERVERT, JournalStatus.OPPLASTING_DOKUMENT, JournalStatus.UKJENT)
+        val journalstatuser: List<Journalstatus> = listOf(Journalstatus.MOTTATT, Journalstatus.JOURNALFOERT, Journalstatus.FERDIGSTILT, Journalstatus.EKSPEDERT, Journalstatus.UNDER_ARBEID, Journalstatus.RESERVERT, Journalstatus.OPPLASTING_DOKUMENT, Journalstatus.UKJENT)
 )
 
 fun hentSafQuery(fnr: String, antallJournalPoster: Int): GraphqlQuery {
