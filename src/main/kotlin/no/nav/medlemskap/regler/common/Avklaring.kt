2d02e13344523bafaf1b2fdf45500225076faa52
Richard Borge <richard.borge@nav.no>
2020-02-14 14:51:59 +0100

    Fjerner ubrukt kode´

--- src/main/kotlin/no/nav/medlemskap/regler/common/Avklaring.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/common/Avklaring.kt
@@ -1,15 +1,8 @@
 package no.nav.medlemskap.regler.common
 
-import no.nav.medlemskap.common.regelCounter
-
 data class Avklaring(
         val identifikator: String,
         val avklaring: String,
         val beskrivelse: String,
         val operasjon: (f: Personfakta) -> Resultat
-) {
-    infix fun evaluerMed(personfakta: Personfakta): Resultat {
-        val resultat = operasjon.invoke(personfakta).apply { regelCounter.labels(this@Avklaring.avklaring.replace("?", ""), this.resultat.name).inc() }
-        return resultat.copy(identifikator = identifikator, avklaring = avklaring)
-    }
-}
+)
