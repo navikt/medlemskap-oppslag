a07815eb7d8a4ae2bbe51d8fcc8db67e40af7444
Richard Borge <richard.borge@nav.no>
2020-02-27 13:06:33 +0100

    Validere request

--- src/main/kotlin/no/nav/medlemskap/routes/EvalueringRoute.kt
+++ src/main/kotlin/no/nav/medlemskap/routes/EvalueringRoute.kt
@@ -2,6 +2,7 @@ package no.nav.medlemskap.routes
 
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.features.BadRequestException
 import io.ktor.features.callId
 import io.ktor.request.receive
 import io.ktor.response.respond
@@ -16,6 +17,7 @@ import no.nav.medlemskap.regler.common.Personfakta
 import no.nav.medlemskap.regler.common.Resultat
 import no.nav.medlemskap.regler.v1.RegelsettForMedlemskap
 import no.nav.medlemskap.services.Services
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
 
@@ -26,7 +28,7 @@ fun Routing.evalueringRoute(
     fun receiveAndRespond() {
         post("/") {
             API_COUNTER.inc()
-            val request = call.receive<Request>()
+            val request = validerRequest(call.receive())
             val callId = call.callId ?: UUID.randomUUID().toString()
             val aktorId = services.pdlService.hentAktorId(request.fnr, callId)
             val datagrunnlag = createDatagrunnlag(
@@ -56,6 +58,34 @@ fun Routing.evalueringRoute(
     } else {
         receiveAndRespond()
     }
+}
+
+private fun validerRequest(request: Request): Request {
+    if (request.periode.tom.isBefore(request.periode.fom)) {
+        throw BadRequestException("Periode tom kan ikke være før periode fom")
+    }
+
+    if (request.periode.fom.isBefore(LocalDate.of(2016, 1, 1))) {
+        throw BadRequestException("Periode fom kan ikke være før 2016-01-01")
+    }
+
+    if (!gyldigFnr(request.fnr)) {
+        throw BadRequestException("Ugyldig fødslesnummer")
+    }
+
+    return request
+}
+
+private fun gyldigFnr(fnr: String): Boolean {
+    if (fnr.length != 11) {
+        return false
+    }
+
+    val s = fnr.toCharArray().map { it.toInt() }
+    val k1 = 11 - ((3 * s[0] + 7 * s[1] + 6 * s[2] + 1 * s[3] + 8 * s[4] + 9 * s[5] + 4 * s[6] + 5 * s[7] + 2 * s[8]) % 11)
+    val k2 = 11 - ((5 * s[0] + 4 * s[1] + 3 * s[2] + 2 * s[3] + 7 * s[4] + 6 * s[5] + 5 * s[6] + 4 * s[7] + 3 * s[8] + 2 * k1) % 11)
+
+    return k1 == s[9] && k2 == s[10]
 }
 
 private suspend fun createDatagrunnlag(
