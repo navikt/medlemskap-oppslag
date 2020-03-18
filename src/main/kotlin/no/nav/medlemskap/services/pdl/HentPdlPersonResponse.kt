be431aa79f641d99f619b6c5cf4e0f03adb55a50
Silje Risvik <silje.risvik@nav.no>
2020-03-18 16:40:39 +0100

    Endret pa null verdier

--- src/main/kotlin/no/nav/medlemskap/services/pdl/HentPdlPersonResponse.kt
+++ src/main/kotlin/no/nav/medlemskap/services/pdl/HentPdlPersonResponse.kt
@@ -6,31 +6,30 @@ data class HentPdlPersonResponse (
         val adressebeskyttelse: List<String>?,
         val kjoenn: List<String>?,
         val navn: List<Navn>?,
-        val bostedsadresse: List<Bostedsadresse>?)
+        val bostedsadresse: List<Bostedsadresse>)
 
 data class Navn (
-        val fornavn: String,
-        val mellomnavn: String?,
-        val etternavn: String )
+        val fornavn: String?,
+        val mellomnavn: String,
+        val etternavn: String? )
 
 data class Bostedsadresse(
-        val angittFlyttedato: LocalDate,
-        val coAdressenavn: String,
-        val vegAdresse: VegAdresse,
-        val adresse: String,
-        val matrikkeladresse: Matrikkeladresse,
-        val ukjentBosted: UkjentBosted,
-        val folkeregisterMetadata: FolkeregisterMetadata?,
-        val metadata: Metadata?)
+        val angittFlyttedato: LocalDate?,
+        val coAdressenavn: String?,
+        val vegAdresse: VegAdresse?,
+        val adresse: String?,
+        val matrikkeladresse: Matrikkeladresse?,
+        val ukjentBosted: UkjentBosted?,
+        val folkeregisterMetadata: FolkeregisterMetadata,
+        val metadata: Metadata)
 
 
 data class Metadata(
-        val opplysningsId: String,
-        val master: String?,
-        val endringer: List<Endring?>?
+        val opplysningsId: String?,
+        val master: String,
+        val endringer: List<Endring>
 
 )
-
 
 enum class Endringstype {
     OPPRETT,
@@ -46,52 +45,43 @@ data class Endring(
         val kilde: String?)
 
 data class FolkeregisterMetadata(
-        val ajourholdstidspunkt: DateTime,
-        val gyldighetstidspunkt: DateTime,
-        val opphoerstidspunkt: DateTime,
-        val kilde:  String,
-        val aarsak: String,
-        val sekvens:  Int
+        val ajourholdstidspunkt: DateTime?,
+        val gyldighetstidspunkt: DateTime?,
+        val opphoerstidspunkt: DateTime?,
+        val kilde:  String?,
+        val aarsak: String?,
+        val sekvens:  Int?
 )
 
 
 data class Matrikkeladresse(
-        val matrikkelId: Int,
-        val bruksenhetsnummer: String,
-        val tilleggsnavn: String,
-        val postnummer: String,
-        val kommunenummer: String,
-        val koordinater: Koordinater
+        val matrikkelId: Int?,
+        val bruksenhetsnummer: String?,
+        val tilleggsnavn: String?,
+        val postnummer: String?,
+        val kommunenummer: String?,
+        val koordinater: Koordinater?
 )
-
-
-
-
-
-
-
 
 data class VegAdresse(
-        val matrikkelId: Int,
-        val husNummer: String,
-        val husBokstav: String,
-        val bruksenhetnummer: String,
-        val adressenavn: String,
-        val kommunenummer: String,
-        val tilleggsnavn: String,
-        val postnummer: String,
-        val koordinater: Koordinater
+        val matrikkelId: Int?,
+        val husNummer: String?,
+        val husBokstav: String?,
+        val bruksenhetnummer: String?,
+        val adressenavn: String?,
+        val kommunenummer: String?,
+        val tilleggsnavn: String?,
+        val postnummer: String?,
+        val koordinater: Koordinater?
 )
-
 
 data class UkjentBosted(
-        val bostedskommune: String
+        val bostedskommune: String?
 )
 
-
 data class Koordinater(
-        val x: Float,
-        val y: Float,
-        val z: Float,
-        val kvalitet: Int
+        val x: Float?,
+        val y: Float?,
+        val z: Float?,
+        val kvalitet: Int?
 )
