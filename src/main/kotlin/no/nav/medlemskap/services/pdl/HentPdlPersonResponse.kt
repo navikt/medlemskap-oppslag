8e861a53efa14ae94651f59421e93f56251c935b
Silje Risvik <silje.risvik@nav.no>
2020-03-26 09:33:50 +0100

    Endret paa domene

--- src/main/kotlin/no/nav/medlemskap/services/pdl/HentPdlPersonResponse.kt
+++ src/main/kotlin/no/nav/medlemskap/services/pdl/HentPdlPersonResponse.kt
@@ -3,12 +3,12 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 
 
-data class HentPdlPersonResponse (val data: Person, val errors: List<PdlError>?)
+data class HentPdlPersonResponse (val data: HentPerson, val errors: List<PdlError>?)
 
+data class HentPerson(val person: Person)
 
 data class Person (
-
-        val adressebeskyttelse: Adressebeskyttelse,
+        val adressebeskyttelse: List<Adressebeskyttelse>,
         val kjoenn: List<String>,
         val navn: List<Navn>,
         val bostedsadresse: List<Bostedsadresse>)
