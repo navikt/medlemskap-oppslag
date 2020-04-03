53dc8b9f410d6f757fe41004e8e4a72d5bbd39f9
Silje Risvik <silje.risvik@nav.no>
2020-04-03 14:37:47 +0200

    La inn periode for norsk arbeidsgiver

--- src/main/kotlin/no/nav/medlemskap/regler/common/Datohjelper.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/common/Datohjelper.kt
@@ -43,6 +43,15 @@ class Datohjelper(val datagrunnlag: Datagrunnlag) {
             )
         }
     }
+
+    fun kontrollPeriodeForNorskarbeidsgiver(): Periode {
+        return when(ytelse) {
+            Ytelse.SYKEPENGER -> Periode(
+                    fom = førsteSykedag().minusMonths(12),
+                    tom = førsteSykedag()
+            )
+        }
+    }
 }
 
 fun lagInterval(periode: Periode): Interval {
