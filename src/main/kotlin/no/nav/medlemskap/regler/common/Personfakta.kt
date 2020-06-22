ae8d9132b757f32347edd6647b4df7c6075d9555
Silje Risvik <silje.risvik@nav.no>
2020-06-22 12:14:00 +0200

    Refaktorering av  adresse og postadresse i tillegg til tester

--- src/main/kotlin/no/nav/medlemskap/regler/common/Personfakta.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/common/Personfakta.kt
@@ -90,11 +90,9 @@ class Personfakta(val datagrunnlag: Datagrunnlag) {
             periodefilter(lagInterval(Periode(it.fom, it.tom)), datohjelper.kontrollPeriodeForNorskAdresse())
 
     fun sjekkBrukersPostadresseOgBostedsadresseLandskode(): Boolean {
-        val harUtenlandskBostedsadresse = !harBrukerNorskBodstedsadresseInnenforSiste12Mnd()
-        val harUtenlandskPostadresse = datagrunnlag.personhistorikk.postadresser.isNotEmpty() &&
-                !harBrukerNorskPostadresseInnenforSiste12Mnd()
-
-        return !(harUtenlandskBostedsadresse || harUtenlandskPostadresse)
+        val harIngenPostadresse = datagrunnlag.personhistorikk.postadresser.isEmpty()
+        return harBrukerNorskPostadresseInnenforSiste12Mnd() || harIngenPostadresse &&
+                harBrukerNorskBodstedsadresseInnenforSiste12Mnd()
     }
 
     fun harBrukerJobbetMerEnnGittStillingsprosentTilEnhverTidIKontrollperiode(gittStillingsprosent: Double): Boolean {
