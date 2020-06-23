e1162d5aeeb68279c619dfb5b83bb1be57722848
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2020-06-23 12:40:56 +0200

    logger claims fremfor payload

--- src/main/kotlin/no/nav/medlemskap/common/JwtConfig.kt
+++ src/main/kotlin/no/nav/medlemskap/common/JwtConfig.kt
@@ -29,7 +29,7 @@ class JwtConfig(val configuration: Configuration, azureAdOpenIdConfiguration: Az
         return try {
             requireNotNull(credentials.payload.audience) { "Auth: Audience mangler i token" }
             require(credentials.payload.audience.contains(configuration.azureAd.jwtAudience)) { "Auth: Ugyldig audience i token" }
-            secureLogger.info { "credentials payload fra JwtConfig: " + credentials.payload }
+            secureLogger.info { "credentials claims fra JwtConfig: " + credentials.payload.claims }
             var azp = credentials.payload.getClaim("azp").asString()
             secureLogger.info("azp verdi fra credentials i JwtConfig: $azp")
             JWTPrincipal(credentials.payload)
