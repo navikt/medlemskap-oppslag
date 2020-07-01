61efddb2ab9d08ab8f1177b05b66638c13442641
Hågen Hasle <haagenhasle@gmail.com>
2020-07-01 17:54:05 +0200

    La til en test for feil angitt ytelse i request

--- src/test/kotlin/no/nav/medlemskap/domene/RequestTest.kt
+++ src/test/kotlin/no/nav/medlemskap/domene/RequestTest.kt
@@ -2,6 +2,7 @@ package no.nav.medlemskap.domene
 
 import assertk.assertThat
 import assertk.assertions.isEqualTo
+import assertk.assertions.isFailure
 import assertk.assertions.isNull
 import com.fasterxml.jackson.module.kotlin.readValue
 import no.nav.medlemskap.common.objectMapper
@@ -52,5 +53,27 @@ internal class RequestTest {
 
         assertThat(objekt.ytelse).isEqualTo(Ytelse.SYKEPENGER)
         assertThat(objekt.fnr).isEqualTo("123456789")
+    }
+
+    @Test
+    fun `mapping skal feile hvis ytelse er angitt feil`() {
+
+        val json = """
+        {
+            "fnr": "123456789",
+            "periode": {
+                "fom": "2019-01-01",
+                "tom": "2019-12-31"
+            },
+            "brukerinput": {
+                "arbeidUtenforNorge": false
+            },
+            "ytelse": "IKKE-SYKEPENGER"
+        }    
+        """.trimIndent()
+
+        assertThat {
+            objectMapper.readValue<Request>(json)
+        }.isFailure()
     }
 }
