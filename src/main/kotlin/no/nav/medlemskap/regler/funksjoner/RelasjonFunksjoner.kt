c8db05bd00eb43485eefcd3c555947b502a6cb96
Silje Risvik <silje.risvik@nav.no>
2020-07-15 12:43:18 +0200

    Laget filtrering for barn under 25

--- src/main/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjoner.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjoner.kt
@@ -4,6 +4,7 @@ import no.bekk.bekkopen.person.FodselsnummerValidator
 import no.nav.medlemskap.domene.*
 import no.nav.medlemskap.regler.common.Funksjoner
 import no.nav.medlemskap.regler.common.lagInterval
+import java.util.*
 
 object RelasjonFunksjoner {
 
@@ -13,18 +14,22 @@ object RelasjonFunksjoner {
 
     //Todo --> Barn hele perioden, er adoptivbarn registrert med annen type
     fun List<Familierelasjon>.hentFnrTilBarnUnder25(): List<String?> =
-            this.filter { it.relatertPersonsRolle == Familierelasjonsrolle.BARN && FodselsnummerValidator.isValid(it.relatertPersonsIdent)}.map {
-                it.relatertPersonsIdent.filtrerBarnUnder25Aar() }
+            this.filter {
+                         it.relatertPersonsRolle == Familierelasjonsrolle.BARN
+                        && FodselsnummerValidator.isValid(it.relatertPersonsIdent)
+                        && it.relatertPersonsIdent.filtrerBarnUnder25Aar()}
+                        .map { it.relatertPersonsIdent}
 
     fun List<PersonhistorikkRelatertPerson>.hentRelatertSomFinnesITPS(relatert: List<String?>?):
             List<PersonhistorikkRelatertPerson> =
-            this.filter { relatert?.contains(it.ident) ?: false && FodselsnummerValidator.isValid(it.ident)}
+            this.filter { relatert?.contains(it.ident) ?: false && FodselsnummerValidator.isValid(it.ident)}}
 
-
-}
-
-//Todo regne ut om barnet er under 25 eller er ikke disse registrert da?
-fun String.filtrerBarnUnder25Aar() = this
+    fun String.filtrerBarnUnder25Aar(): Boolean {
+        val aar = Calendar.getInstance().get(Calendar.YEAR)
+        val foedselsaar = this.substring(3, 5).toInt()
+         val alder = aar - foedselsaar
+        return alder >= 25
+    }
 
 //Todo sjekke tom på sivilstand
 fun Sivilstand.sivilstandPeriodeOverlapperKontrollPerioden(kontrollPeriode: Periode) =
