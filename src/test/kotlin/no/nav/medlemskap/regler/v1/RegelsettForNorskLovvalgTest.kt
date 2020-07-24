7d6d562d43fdc19dc552c8d6cd81292cc7e3f551
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-07-24 08:41:32 +0200

    La til Cucumber-tester for regelflyt 11.4 nei

--- src/test/kotlin/no/nav/medlemskap/regler/v1/RegelsettForNorskLovvalgTest.kt
+++ src/test/kotlin/no/nav/medlemskap/regler/v1/RegelsettForNorskLovvalgTest.kt
@@ -1,7 +1,8 @@
 package no.nav.medlemskap.regler.v1
 
 import no.nav.medlemskap.regler.assertSvar
-import no.nav.medlemskap.regler.common.RegelId.*
+import no.nav.medlemskap.regler.common.RegelId.REGEL_11_4
+import no.nav.medlemskap.regler.common.RegelId.REGEL_11_5
 import no.nav.medlemskap.regler.common.Svar
 import no.nav.medlemskap.regler.evaluer
 import no.nav.medlemskap.regler.personer.Personleser
@@ -12,26 +13,10 @@ class RegelsettForNorskLovvalgTest {
     private val personleser = Personleser()
 
 
-    @Test
-    fun `person med ektefelle får ja`() {
-        assertSvar(REGEL_11_2, Svar.JA, evaluer(personleser.brukerHarEktefelle()), Svar.JA)
-    }
-
    /* fun `Bruker over 25 regnes ikke som barn`() {
         assertSvar("11.2.1", Svar.NEI, evaluer(personleser.brukerHarBarnOver25RegnesIkkeSomBarn()), Svar.JA)
     }*/
 
-
-
-    @Test
-    fun `person med ektefelle og barn får ja`() {
-        assertSvar(REGEL_11_3, Svar.JA, evaluer(personleser.brukerHarEktefelleOgBarn()), Svar.JA)
-    }
-
-    @Test
-    fun `person med ektefelle og ikke barn får nei`() {
-        assertSvar(REGEL_11_3, Svar.NEI, evaluer(personleser.brukerHarEktefelleMenIkkeBarn()), Svar.JA)
-    }
 
     @Test
     fun `person med barn har ektefelle registrert i Norge får ja`() {
@@ -43,15 +28,7 @@ class RegelsettForNorskLovvalgTest {
         assertSvar(REGEL_11_4, Svar.NEI, evaluer(personleser.brukerHarBarnOgEktefelleErIkkeRegistrertINorge()), Svar.JA)
     }
 
-    @Test
-    fun `person uten folkeregistrert ektefelle som har folkeregistrert barn får ja`() {
-        assertSvar(REGEL_11_4_1, Svar.JA, evaluer(personleser.brukerHarIkkeFolkeregistrertEktefelleMenFolkeregistrertBarn()), Svar.UAVKLART)
-    }
 
-    @Test
-    fun `person uten folkeregistrert ektefelle som ikke folkeregistrert barn får nei`() {
-        assertSvar(REGEL_11_4_1, Svar.NEI, evaluer(personleser.brukerHarIkkeFolkeregistrertEktefelleOgIkkeFolkeregisrertBarn()), Svar.JA)
-    }
     @Test
     fun `person med barn og ektefelle registrert i Norge får ja`() {
         assertSvar(REGEL_11_5, Svar.JA, evaluer(personleser.brukerHarFolkeregistrertEktefelleOgBarn()), Svar.JA)
