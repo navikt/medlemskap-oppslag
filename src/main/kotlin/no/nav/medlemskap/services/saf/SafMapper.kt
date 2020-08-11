2c1e9dac113c53724168629d3694d66b0f8961c6
Hågen Hasle <haagenhasle@gmail.com>
2020-08-11 19:37:13 +0200

    Byttet håndskreven kode for SafClient med kode generert av expediagroups graphql-kotlin-client

--- src/main/kotlin/no/nav/medlemskap/services/saf/SafMapper.kt
+++ src/main/kotlin/no/nav/medlemskap/services/saf/SafMapper.kt
@@ -1,26 +1,28 @@
 package no.nav.medlemskap.services.saf
 
+import no.nav.medlemskap.client.generated.Dokumenter
 import no.nav.medlemskap.domene.Dokument
 import no.nav.medlemskap.domene.Journalpost
 
-fun mapDokumentoversiktBrukerResponse(response: DokumentoversiktBrukerResponse): List<Journalpost> =
-        response.data?.let { mapJournalResultat(it.dokumentoversiktBruker.journalposter) } ?: listOf()
+fun mapDokumentoversiktBrukerResponse(response: Dokumenter.Result): List<Journalpost> =
+        mapJournalResultat(response.dokumentoversiktBruker.journalposter)
 
-fun mapJournalResultat(journal: List<JournalPost>): List<Journalpost> {
-    return  journal.map {
-       Journalpost(
-               dokumenter = mapDokumenter(it),
-               journalpostId = it.journalpostId,
-               journalposttype = it.journalposttype.toString(),
-               journalstatus = it.journalstatus.toString(),
-               tema = it.tema.toString(),
-               tittel = it.tittel)
+fun mapJournalResultat(journal: List<Dokumenter.Journalpost?>): List<Journalpost> {
+    return journal.filterNotNull().map {
+        Journalpost(
+                dokumenter = mapDokumenter(it),
+                journalpostId = it.journalpostId,
+                journalposttype = it.journalposttype.toString(),
+                journalstatus = it.journalstatus.toString(),
+                tema = it.tema.toString(),
+                tittel = it.tittel)
     }
 }
 
-fun mapDokumenter(journalPost: JournalPost): List<Dokument>? {
-    return journalPost.dokumenter?.map {
-        Dokument(dokumentId = it.dokumentInfoId, tittel = it.tittel) }
+fun mapDokumenter(journalPost: Dokumenter.Journalpost): List<Dokument> {
+    return journalPost.dokumenter?.filterNotNull()?.map {
+        Dokument(dokumentId = it.dokumentInfoId, tittel = it.tittel)
+    } ?: emptyList()
 }
 
 
