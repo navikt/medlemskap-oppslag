d5098fac3e1b7543bd81795ecd3a6b5cad3e0a1f
Hågen Hasle <hagen.hasle@nav.no>
2020-08-18 11:31:33 +0200

    Lagt til oss selv som ytelse i dev, så vi kan kalle vår egen tjeneste for å teste (#207)

--- src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
@@ -3,7 +3,8 @@ package no.nav.medlemskap.domene
 enum class Ytelse {
     SYKEPENGER,
     DAGPENGER,
-    ENSLIG_FORSORGER;
+    ENSLIG_FORSORGER,
+    LOVME;
 
     companion object {
 
@@ -13,7 +14,8 @@ enum class Ytelse {
                 "337777ed-eced-4765-9504-c6e7aebec9e5" to DAGPENGER, //Prod - dp-oppslag-medlemskap
                 "1e8e8e29-a308-455f-b744-adcb4d2fe763" to DAGPENGER, //dev
                 "4de2bae4-f86a-4c12-aeef-74177c6e724a" to ENSLIG_FORSORGER, //Prod - familie-ef-soknad-api
-                "ea03f30a-413b-4f0e-bfae-477aad57de67" to ENSLIG_FORSORGER //dev
+                "ea03f30a-413b-4f0e-bfae-477aad57de67" to ENSLIG_FORSORGER, //dev
+                "496b0ded-cfdb-4430-b7b6-b568504de005" to LOVME//dev
         )
 
         fun fromClientId(clientId: String?): Ytelse? = clientIdToYtelseMap[clientId]
