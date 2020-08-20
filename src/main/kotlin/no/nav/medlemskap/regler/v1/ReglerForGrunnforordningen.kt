aa1187932c5dfab4e5106bd51aca2c505b8cdc1e
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-08-20 09:24:49 +0200

    Refaktorerte ReglerForGrunnforordningen til å sette opp regler i RegelMap.

--- src/main/kotlin/no/nav/medlemskap/regler/v1/ReglerForGrunnforordningen.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/v1/ReglerForGrunnforordningen.kt
@@ -1,23 +1,18 @@
 package no.nav.medlemskap.regler.v1
 
 import no.nav.medlemskap.domene.Datagrunnlag
-import no.nav.medlemskap.domene.InputPeriode
-import no.nav.medlemskap.domene.Statsborgerskap
 import no.nav.medlemskap.domene.Ytelse
-import no.nav.medlemskap.regler.common.Regelflyt
-import no.nav.medlemskap.regler.common.Regler
-import no.nav.medlemskap.regler.common.regelFlytUavklart
+import no.nav.medlemskap.regler.common.*
 
 class ReglerForGrunnforordningen(
         ytelse: Ytelse,
-        val periode: InputPeriode,
-        val statsborgerskap: List<Statsborgerskap>,
-        val reglerForArbeidsForhold: ReglerForArbeidsforhold
-) : Regler(ytelse) {
+        private val reglerForArbeidsForhold: ReglerForArbeidsforhold,
+        regelMap: Map<RegelId, Regel>
+) : Regler(ytelse, regelMap) {
 
     override fun hentRegelflyt(): Regelflyt {
         val erBrukerEØSborgerFlyt = lagRegelflyt(
-                regel = erBrukerEØSborger.regel,
+                regel = hentRegel(RegelId.REGEL_2),
                 hvisJa = reglerForArbeidsForhold.hentRegelflyt(),
                 hvisNei = regelFlytUavklart(ytelse)
         )
@@ -25,20 +20,21 @@ class ReglerForGrunnforordningen(
         return erBrukerEØSborgerFlyt
     }
 
-    val erBrukerEØSborger = ErBrukerEøsBorgerRegel(
-            ytelse = ytelse,
-            periode = periode,
-            statsborgerskap = statsborgerskap
-    )
-
     companion object {
         fun fraDatagrunnlag(datagrunnlag: Datagrunnlag): ReglerForGrunnforordningen {
             return ReglerForGrunnforordningen(
                     ytelse = datagrunnlag.ytelse,
-                    periode = datagrunnlag.periode,
-                    statsborgerskap = datagrunnlag.personhistorikk.statsborgerskap,
-                    reglerForArbeidsForhold = ReglerForArbeidsforhold.fraDatagrunnlag(datagrunnlag)
+                    reglerForArbeidsForhold = ReglerForArbeidsforhold.fraDatagrunnlag(datagrunnlag),
+                    regelMap = lagRegelMap(datagrunnlag)
             )
+        }
+
+        private fun lagRegelMap(datagrunnlag: Datagrunnlag): Map<RegelId, Regel> {
+            val regelListe = listOf(
+                    ErBrukerEøsBorgerRegel.fraDatagrunnlag(datagrunnlag)
+            )
+
+            return regelListe.map { it.regelId to it.regel }.toMap()
         }
     }
 }
