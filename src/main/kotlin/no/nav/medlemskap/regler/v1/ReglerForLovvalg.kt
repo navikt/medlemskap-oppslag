4819d2639fb1f3568ebb2357b3667b6fa193ffd7
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-08-28 10:48:45 +0200

    Lagde regelsett for ReglerForEøsBorgere.

--- src/main/kotlin/no/nav/medlemskap/regler/v1/ReglerForLovvalg.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/v1/ReglerForLovvalg.kt
@@ -5,7 +5,10 @@ import no.nav.medlemskap.domene.InputPeriode
 import no.nav.medlemskap.domene.Ytelse
 import no.nav.medlemskap.regler.common.*
 import no.nav.medlemskap.regler.common.RegelId.*
-import no.nav.medlemskap.regler.v1.lovvalg.*
+import no.nav.medlemskap.regler.v1.lovvalg.ErBrukerBosattINorgeRegel
+import no.nav.medlemskap.regler.v1.lovvalg.HarBrukerJobbet25ProsentEllerMerRegel
+import no.nav.medlemskap.regler.v1.lovvalg.HarBrukerJobbetUtenforNorgeRegel
+import no.nav.medlemskap.regler.v1.lovvalg.HarBrukerNorskStatsborgerskapRegel
 
 class ReglerForLovvalg(
         val periode: InputPeriode,
@@ -59,23 +62,9 @@ class ReglerForLovvalg(
         private fun lagRegelMap(datagrunnlag: Datagrunnlag): Map<RegelId, Regel> {
             val regelListe = listOf(
                     ErBrukerBosattINorgeRegel.fraDatagrunnlag(datagrunnlag),
-                    ErBrukersEktefelleBosattINorgeRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_4),
-                    ErBrukersEktefelleBosattINorgeRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_3_1),
-                    HarBrukersEktefelleJobbet100ProsentEllerMerRegel.fraDatagrunnlag(datagrunnlag),
-                    ErBrukersEktefelleOgBarnasMorSammePersonRegel.fraDatagrunnlag(datagrunnlag),
-                    HarBrukerBarnSomErFolkeregistrertRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_2_2),
-                    HarBrukerBarnSomErFolkeregistrertRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_4_1),
-                    HarBrukerBarnSomErFolkeregistrertRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_5),
-                    HarBrukerBarnRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_3),
-                    HarBrukerBarnRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_2_1),
                     HarBrukerJobbetUtenforNorgeRegel.fraDatagrunnlag(datagrunnlag),
                     HarBrukerNorskStatsborgerskapRegel.fraDatagrunnlag(datagrunnlag),
-                    HarBrukerJobbet25ProsentEllerMerRegel.fraDatagrunnlag(datagrunnlag),
-                    HarBrukerEktefelleRegel.fraDatagrunnlag(datagrunnlag),
-                    HarBrukerJobbet80ProsentEllerMerRegel.fraDatagrunnlag (datagrunnlag, REGEL_11_6),
-                    HarBrukerJobbet80ProsentEllerMerRegel.fraDatagrunnlag (datagrunnlag, REGEL_11_2_3),
-                    HarBrukerJobbet100ProsentEllerMerRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_3_1_1),
-                    HarBrukerJobbet100ProsentEllerMerRegel.fraDatagrunnlag(datagrunnlag, REGEL_11_2_2_1)
+                    HarBrukerJobbet25ProsentEllerMerRegel.fraDatagrunnlag(datagrunnlag)
             )
 
             return regelListe.map { it.regelId to it.regel }.toMap()
