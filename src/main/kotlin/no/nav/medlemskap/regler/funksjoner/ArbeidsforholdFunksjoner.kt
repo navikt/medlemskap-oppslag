79c8cc8271e840190e8127d7f80e46314f915f6f
StianSDouzette <59877023+StianSDouzette@users.noreply.github.com>
2020-09-08 10:11:28 +0200

    Orgnummer grafana (#240)
    * Filtrer ut færre enn 6 ansatte i Grafana
    
    * Tester på filtrering av arbeidsgivere med færre enn 6 ansatte
    
    * Merge from master
    
    * Fjerner utkommentert kode
    
    Co-authored-by: Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
    Co-authored-by: Stian S. Douzette <stians.douzette@nav.no>

--- src/main/kotlin/no/nav/medlemskap/regler/funksjoner/ArbeidsforholdFunksjoner.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/funksjoner/ArbeidsforholdFunksjoner.kt
@@ -5,7 +5,6 @@ import no.nav.medlemskap.domene.*
 import no.nav.medlemskap.domene.Ytelse.Companion.metricName
 import no.nav.medlemskap.regler.common.Funksjoner
 import no.nav.medlemskap.regler.common.Funksjoner.antall
-import no.nav.medlemskap.regler.common.Funksjoner.finnesMindreEnn
 import no.nav.medlemskap.regler.common.erDatoerSammenhengende
 import no.nav.medlemskap.regler.common.interval
 import no.nav.medlemskap.regler.common.lagInterval
@@ -21,8 +20,8 @@ object ArbeidsforholdFunksjoner {
         return arbeidsforholdForKontrollPeriode(kontrollPeriode).stream().allMatch { it.arbeidsgivertype == OpplysningspliktigArbeidsgiverType.Organisasjon }
     }
 
-    infix fun List<Arbeidsforhold>.antallAnsatteHosArbeidsgivere(kontrollPeriode: Kontrollperiode): List<Int?> =
-        ansatteHosArbeidsgivere(kontrollPeriode).map { it.antall }
+    infix fun List<Arbeidsforhold>.antallAnsatteHosArbeidsgivere(kontrollPeriode: Kontrollperiode): List<Int> =
+        ansatteHosArbeidsgivere(kontrollPeriode).map { it.antall ?: 0 }
 
     infix fun List<Arbeidsforhold>.arbeidsforholdForYrkestype(kontrollPeriode: Kontrollperiode): List<String> =
         this.filter {
@@ -43,9 +42,15 @@ object ArbeidsforholdFunksjoner {
         return arbeidsforholdForKontrollPeriode(kontrollPeriode).flatMap { it.arbeidsgiver.konkursStatus.orEmpty() }
     }
 
-    fun List<Arbeidsforhold>.registrerArbeidsgiver(kontrollPeriode: Kontrollperiode, ytelse: Ytelse) =
-        this.filter { antallAnsatteHosArbeidsgivere(kontrollPeriode) finnesMindreEnn 6 }
-            .forEach { antallTreffPåArbeidsgiver(it.arbeidsgiver.identifikator, ytelse).increment() }
+    fun List<Arbeidsforhold>.filtrerUtArbeidsgivereMedFærreEnn6Ansatte(kontrollPeriode: Kontrollperiode) =
+        arbeidsforholdForKontrollPeriode(kontrollPeriode).stream().map { it.arbeidsgiver }.collect(Collectors.toList())
+            .filter { !it.ansatte?.finnesMindreEnn(6).isNullOrEmpty() }
+
+    fun List<Arbeidsgiver>.registrereArbeidsgivere(ytelse: Ytelse) {
+        this.forEach { antallTreffPåArbeidsgiver(it.identifikator, ytelse).increment() }
+    }
+
+    fun List<Ansatte>.finnesMindreEnn(tall: Int) = this.filter { it.antall ?: 0 < tall }
 
     fun List<Arbeidsforhold>.registrerAntallAnsatte(ytelse: Ytelse) =
         this.forEach {
