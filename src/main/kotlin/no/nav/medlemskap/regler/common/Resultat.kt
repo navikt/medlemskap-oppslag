78ad6761ce585b6722feea1d5ad69a67ce5b7209
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-09-14 10:44:39 +0200

    La til metode i Resultat for å finne resultat for regel

--- src/main/kotlin/no/nav/medlemskap/regler/common/Resultat.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/common/Resultat.kt
@@ -24,4 +24,29 @@ data class Resultat(
     fun erKonklusjon(): Boolean {
         return erMedlemskonklusjon() || erRegelflytKonklusjon()
     }
+
+    fun finnRegelResultat(regelId: RegelId): Resultat? {
+        val regelResultat = delresultat.find { it.regelId == regelId }
+        if (regelResultat != null) {
+            return regelResultat
+        }
+
+        return regelResultat
+    }
+
+    private fun finnRegelResultat(resultat: Resultat, regelId: RegelId): Resultat? {
+        var regelResultat = resultat.delresultat.find { it.regelId == regelId }
+        if (regelResultat != null) {
+            return regelResultat
+        }
+
+        resultat.delresultat.forEach{ delresultat ->
+            regelResultat = delresultat.delresultat.find { it.regelId == regelId }
+            if (regelResultat != null) {
+                return regelResultat
+            }
+        }
+
+        return regelResultat
+    }
 }
