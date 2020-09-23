3585ffe5368e662b74e2a095a66ea99aa79d1b6d
Stian S. Douzette <stians.douzette@nav.no>
2020-09-23 11:17:08 +0200

    Tydligere metodenavn

--- src/main/kotlin/no/nav/medlemskap/common/Metrics.kt
+++ src/main/kotlin/no/nav/medlemskap/common/Metrics.kt
@@ -152,7 +152,7 @@ fun flereStatsborgerskapCounter(antallStatsborgerskap: String, ytelse: Ytelse): 
 
 fun endretStatsborgerskapSisteÅretCounter(statsborgerskapEndretSisteÅret: Boolean, ytelse: Ytelse): Counter = Counter
     .builder("endret_statsborgerskap_siste_aaret")
-    .tags("endret_statsborgerskap", endretStatsborgerskap(statsborgerskapEndretSisteÅret), "ytelse", ytelse.metricName())
+    .tags("endret_statsborgerskap", mapEndretStatsborgerskapBooleanTilMetrikkVerdi(statsborgerskapEndretSisteÅret), "ytelse", ytelse.metricName())
     .description("")
     .register(Metrics.globalRegistry)
 
@@ -174,11 +174,11 @@ fun enhetstypeForJuridiskEnhet(enhetstype: String?, ytelse: String): Counter = C
     .description("Ulike enhetstyper for juridiske enheter")
     .register(Metrics.globalRegistry)
 
-private fun endretStatsborgerskap(statsborgerskapEndretSisteÅret: Boolean) : String =
-    when(statsborgerskapEndretSisteÅret) {
+private fun mapEndretStatsborgerskapBooleanTilMetrikkVerdi (statsborgerskapEndretSisteÅret: Boolean): String =
+    when (statsborgerskapEndretSisteÅret) {
         true -> "endret"
         false -> "uendret"
-}
+    }
 
 private fun getStillingsprosentIntervall(stillingsprosent: Double): String {
     // Fjerner desimaler fremfor å runde av fordi regelsjekken godtar ikke f.eks. 24.9% stilling som høy nok til å regnes som 25% stilling.
