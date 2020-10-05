98cbfe8e735eb47db1e0204b475cb215cc082e35
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-10-05 14:32:43 +0200

    La til sammensatt eksempel på mapping fra PDL til datagrunnlag

--- src/test/kotlin/no/nav/medlemskap/cucumber/steps/PdlMapperSteps.kt
+++ src/test/kotlin/no/nav/medlemskap/cucumber/steps/PdlMapperSteps.kt
@@ -6,118 +6,131 @@ import io.kotest.matchers.collections.shouldContainExactly
 import no.nav.medlemskap.clients.pdl.generated.HentPerson
 import no.nav.medlemskap.cucumber.*
 import no.nav.medlemskap.cucumber.mapping.pdl.PdlDomenespråkParser
-import no.nav.medlemskap.domene.Adresse
-import no.nav.medlemskap.domene.Familierelasjon
-import no.nav.medlemskap.domene.Sivilstand
-import no.nav.medlemskap.domene.Statsborgerskap
-import no.nav.medlemskap.services.pdl.PdlSivilstandMapper.mapSivilstander
-import no.nav.medlemskap.services.pdl.mapper.PdlMapper.mapBostedsadresser
-import no.nav.medlemskap.services.pdl.mapper.PdlMapper.mapFamilierelasjoner
-import no.nav.medlemskap.services.pdl.mapper.PdlMapper.mapKontaktAdresser
-import no.nav.medlemskap.services.pdl.mapper.PdlMapper.mapOppholdsadresser
-import no.nav.medlemskap.services.pdl.mapper.PdlMapper.mapStatsborgerskap
+import no.nav.medlemskap.domene.Personhistorikk
+import no.nav.medlemskap.services.pdl.mapper.PdlMapper
 
 class PdlMapperSteps : No {
     private val pdlDomenespråkParser = PdlDomenespråkParser()
     private val domenespråkParser = DomenespråkParser()
 
-    private var pdlStatsborgerskap: List<HentPerson.Statsborgerskap> = emptyList()
-    private var pdlBostedsadresser: List<HentPerson.Bostedsadresse> = emptyList()
-    private var pdlKontaktadresser: List<HentPerson.Kontaktadresse> = emptyList()
-    private var pdlOppholdsadresser: List<HentPerson.Oppholdsadresse> = emptyList()
-    private var pdlSivilstander: List<HentPerson.Sivilstand> = emptyList()
-    private var pdlFamilierelasjoner: List<HentPerson.Familierelasjon> = emptyList()
+    private var pdlPersonBuilder = PdlPersonBuilder()
 
-    private var statsborgerskap: List<Statsborgerskap> = emptyList()
-    private var bostedsadresser: List<Adresse> = emptyList()
-    private var kontaktadresser: List<Adresse> = emptyList()
-    private var oppholdsadresser: List<Adresse> = emptyList()
-    private var sivilstander: List<Sivilstand> = emptyList()
-    private var familierelasjoner: List<Familierelasjon> = emptyList()
+    private var personhistorikk: Personhistorikk? = null
 
     init {
         Gitt<DataTable>("følgende statsborgerskap fra PDL:") { dataTable: DataTable? ->
-            pdlStatsborgerskap = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.StatsborgerskapMapper())
+            pdlPersonBuilder.statsborgerskap = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.StatsborgerskapMapper())
         }
 
         Gitt<DataTable>("følgende bostedsadresser fra PDL:") { dataTable: DataTable? ->
-            pdlBostedsadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.BostedsadresseMapper())
+            pdlPersonBuilder.bostedsadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.BostedsadresseMapper())
         }
 
         Gitt<DataTable>("følgende kontaktadresser fra PDL:") { dataTable: DataTable? ->
-            pdlKontaktadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.KontaktadresseMapper())
+            pdlPersonBuilder.kontaktadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.KontaktadresseMapper())
         }
 
         Gitt<DataTable>("følgende oppholdsadresser fra PDL:") { dataTable: DataTable? ->
-            pdlOppholdsadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.OppholdsadresseMapper())
+            pdlPersonBuilder.oppholdsadresser = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.OppholdsadresseMapper())
         }
 
         Gitt<DataTable>("følgende sivilstander fra PDL:") { dataTable: DataTable? ->
-            pdlSivilstander = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.SivilstandMapper())
+            pdlPersonBuilder.sivilstander = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.SivilstandMapper())
         }
 
         Gitt<DataTable>("følgende familierelasjoner fra PDL:") { dataTable: DataTable? ->
-            pdlFamilierelasjoner = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.FamilerelasjonMapper())
+            pdlPersonBuilder.familierelasjoner = pdlDomenespråkParser.mapDataTable(dataTable, PdlDomenespråkParser.FamilerelasjonMapper())
         }
 
         Når("statsborgerskap mappes") {
-            statsborgerskap = mapStatsborgerskap(pdlStatsborgerskap)
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Når("bostedsadresser mappes") {
-            bostedsadresser = mapBostedsadresser(pdlBostedsadresser)
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Når("kontaktadresser mappes") {
-            kontaktadresser = mapKontaktAdresser(pdlKontaktadresser)
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Når("oppholdsadresser mappes") {
-            oppholdsadresser = mapOppholdsadresser(pdlOppholdsadresser)
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Når("sivilstander mappes") {
-            sivilstander = mapSivilstander(pdlSivilstander)
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Når("familierelasjoner mappes") {
-            familierelasjoner = mapFamilierelasjoner(pdlFamilierelasjoner)
+            personhistorikk = mapTilPersonhistorikk()
+        }
+
+        Når("PDL Person mappes til personhistorikk i datagrunnlaget") {
+            personhistorikk = mapTilPersonhistorikk()
         }
 
         Så<DataTable>("skal mappet statsborgerskap være") { dataTable: DataTable? ->
             val statsborgerskapForventet = domenespråkParser.mapDataTable(dataTable, StatsborgerskapMapper())
 
-            statsborgerskap.shouldContainExactly(statsborgerskapForventet)
+            personhistorikk!!.statsborgerskap.shouldContainExactly(statsborgerskapForventet)
         }
 
         Så<DataTable>("skal mappede bostedsadresser være") { dataTable: DataTable? ->
             val bostedsadresserForventet = domenespråkParser.mapDataTable(dataTable, AdresseMapper())
 
-            bostedsadresser.shouldContainExactly(bostedsadresserForventet)
+            personhistorikk!!.bostedsadresser.shouldContainExactly(bostedsadresserForventet)
         }
 
         Så<DataTable>("skal mappede kontaktadresser være") { dataTable: DataTable? ->
             val kontaktadresserForventet = domenespråkParser.mapDataTable(dataTable, AdresseMapper())
 
-            kontaktadresser.shouldContainExactly(kontaktadresserForventet)
+            personhistorikk!!.kontaktadresser.shouldContainExactly(kontaktadresserForventet)
         }
 
         Så<DataTable>("skal mappede oppholdsadresser være") { dataTable: DataTable? ->
             val oppholdsadresserForventet = domenespråkParser.mapDataTable(dataTable, AdresseMapper())
 
-            oppholdsadresser.shouldContainExactly(oppholdsadresserForventet)
+            personhistorikk!!.oppholdsadresser.shouldContainExactly(oppholdsadresserForventet)
         }
 
         Så<DataTable>("skal mappede sivilstander være") { dataTable: DataTable? ->
             val sivilstanderForventet = domenespråkParser.mapDataTable(dataTable, SivilstandMapper())
 
-            sivilstander.shouldContainExactly(sivilstanderForventet)
+            personhistorikk!!.sivilstand.shouldContainExactly(sivilstanderForventet)
         }
 
         Så<DataTable>("skal mappede familierelasjoner være") { dataTable: DataTable? ->
             val familierelasjonerForventet = domenespråkParser.mapDataTable(dataTable, FamilieRelasjonMapper())
 
-            familierelasjoner.shouldContainExactly(familierelasjonerForventet)
+            personhistorikk!!.familierelasjoner.shouldContainExactly(familierelasjonerForventet)
+        }
+
+        Så<DataTable>("skal personhistorikk.familierelasjoner være") { dataTable: DataTable? ->
+        }
+    }
+
+    private fun mapTilPersonhistorikk(): Personhistorikk {
+        return PdlMapper.mapTilPersonHistorikkTilBruker(pdlPersonBuilder.build())
+    }
+
+    class PdlPersonBuilder {
+        var statsborgerskap: List<HentPerson.Statsborgerskap> = emptyList()
+        var bostedsadresser: List<HentPerson.Bostedsadresse> = emptyList()
+        var kontaktadresser: List<HentPerson.Kontaktadresse> = emptyList()
+        var oppholdsadresser: List<HentPerson.Oppholdsadresse> = emptyList()
+        var sivilstander: List<HentPerson.Sivilstand> = emptyList()
+        var familierelasjoner: List<HentPerson.Familierelasjon> = emptyList()
+
+        fun build(): HentPerson.Person {
+            return HentPerson.Person(
+                familierelasjoner = familierelasjoner,
+                statsborgerskap = statsborgerskap,
+                sivilstand = sivilstander,
+                bostedsadresse = bostedsadresser,
+                kontaktadresse = kontaktadresser,
+                oppholdsadresse = oppholdsadresser
+            )
         }
     }
 }
