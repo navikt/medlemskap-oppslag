de27b47f458de09d17d06ab79bb11217cdf60e52
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-12-15 17:14:37 +0100

    Fikset ServiceName

--- src/main/kotlin/no/nav/medlemskap/clients/udi/UdiFactory.kt
+++ src/main/kotlin/no/nav/medlemskap/clients/udi/UdiFactory.kt
@@ -11,7 +11,7 @@ object UdiFactory {
     private val ServiceClass = MT1067NAVV1Interface::class.java
     private const val Wsdl = "wsdl/MT_1067_NAV_v1.wsdl"
     private const val Namespace = "http://udi.no.MT_1067_NAV.v1"
-    private val ServiceName = QName(Namespace, "MT_1067_NAV_v1Service")
+    private val ServiceName = QName(Namespace, "MT_1067_NAV_v1Port")
     private val EndpointName = QName(Namespace, "MT_1067_NAV_v1Port")
 
     fun create(endpointUrl: String, features: List<Feature> = emptyList(), outInterceptors: List<Interceptor<Message>> = emptyList()) =
