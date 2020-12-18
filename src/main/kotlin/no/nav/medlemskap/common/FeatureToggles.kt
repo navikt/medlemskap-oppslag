e3a3ba6aec252b396ebac6870de70f1044e11f1a
Per Birger Eriksen <per.birger.eriksen@nav.no>
2020-12-18 08:31:43 +0100

    Kommenterte ut helsesjekk på udiClient

--- src/main/kotlin/no/nav/medlemskap/common/FeatureToggles.kt
+++ src/main/kotlin/no/nav/medlemskap/common/FeatureToggles.kt
@@ -1,5 +1,5 @@
 package no.nav.medlemskap.common
 
-enum class FeatureToggles(val enabled: Boolean = false, val description: String = "") {
+enum class FeatureToggles(val enabled: Boolean = false, private val description: String = "") {
     FEATURE_UDI(false, "Kan ikke enables før vi får til å kalle UDI-tjenesten")
 }
