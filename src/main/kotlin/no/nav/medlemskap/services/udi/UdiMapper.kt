9083cc134167f668ce8ba63cfd4b227f3a60d318
Silje Risvik <silje.risvik@nav.no>
2020-12-18 12:09:44 +0100

    Udi mapping

--- src/main/kotlin/no/nav/medlemskap/services/udi/UdiMapper.kt
+++ src/main/kotlin/no/nav/medlemskap/services/udi/UdiMapper.kt
@@ -1,6 +1,6 @@
 package no.nav.medlemskap.services.udi
 import no.nav.medlemskap.domene.Oppholdstillatelse
-import no.udi.mt_1067_nav_data.v1.HentPersonstatusResultat
+import no.udi.mt_1067_nav_data.v1.*
 import java.time.LocalDateTime
 import javax.xml.datatype.XMLGregorianCalendar
 
@@ -9,12 +9,40 @@ object UdiMapper {
         return Oppholdstillatelse(
             uttrekkstidspunkt = oppholdstillatelse.uttrekkstidspunkt.asDate(),
             foresporselsfodselsnummer = oppholdstillatelse.foresporselsfodselsnummer,
-            avgjoerelse = oppholdstillatelse.avgjorelsehistorikk.isUavklart,
-            uavklartFlyktningstatus = oppholdstillatelse.isUavklartFlyktningstatus,
-            gjeldendeOppholdsstatus = null,
-            harFlyktningstatus = oppholdstillatelse.isHarFlyktningstatus,
+            gjeldendeOppholdsstatus = mapGjeldendeOppholdsstatus(oppholdstillatelse.gjeldendeOppholdsstatus),
+            avgjoerelse = null,
+            harFlyktningstatus = null,
+            uavklartFlyktningstatus = null ,
             arbeidsadgang = null
         )
+    }
+
+    private fun mapGjeldendeOppholdsstatus(gjeldendeOppholdsstatus: GjeldendeOppholdsstatus): Boolean {
+        if(gjeldendeOppholdsstatus.eoSellerEFTAOpphold != null){
+            mapEosEllerEftaopphold(gjeldendeOppholdsstatus.eoSellerEFTAOpphold)
+        }
+        if(gjeldendeOppholdsstatus.oppholdstillatelseEllerOppholdsPaSammeVilkar != null){
+            mapOppholdstillatelseEllerOppholdsPaSammeVilkar(gjeldendeOppholdsstatus.oppholdstillatelseEllerOppholdsPaSammeVilkar)
+        }
+        if(gjeldendeOppholdsstatus.ikkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum != null){
+            mapIkkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum(gjeldendeOppholdsstatus.ikkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum)
+        }
+        if(gjeldendeOppholdsstatus.uavklart != null){
+            return false
+        }
+
+    }
+
+    private fun mapIkkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum(ikkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum: IkkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun mapOppholdstillatelseEllerOppholdsPaSammeVilkar(oppholdstillatelseEllerOppholdsPaSammeVilkar: OppholdstillatelseEllerOppholdsPaSammeVilkar?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun mapEosEllerEftaopphold(eoSellerEFTAOpphold: EOSellerEFTAOpphold?) {
+
     }
 
     private fun XMLGregorianCalendar?.asDate(): LocalDateTime? = this?.toGregorianCalendar()?.toZonedDateTime()?.toLocalDateTime()
