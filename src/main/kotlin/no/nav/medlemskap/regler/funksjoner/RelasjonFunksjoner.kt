6b3a04f61dc2f39385f6d8c444534381b10bffdf
Per Birger Eriksen <65544832+navperbirg@users.noreply.github.com>
2021-01-04 12:01:33 +0100

    Sjekk på om barn er under 25 år bør bruke inputdato istedet for Calendar. (#385)

--- src/main/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjoner.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjoner.kt
@@ -6,12 +6,12 @@ import no.nav.medlemskap.domene.Familierelasjonsrolle
 import no.nav.medlemskap.domene.Fødselsnummer.Companion.hentBursdagsAar
 import no.nav.medlemskap.domene.Personhistorikk
 import no.nav.medlemskap.domene.Sivilstandstype
-import java.util.*
+import java.time.LocalDate
 
 object RelasjonFunksjoner {
 
-    fun String.erBarnUnder25Aar() =
-        (Calendar.getInstance().get(Calendar.YEAR) - this.hentBursdagsAar().toInt()) <= 25
+    fun String.erBarnUnder25Aar(førsteDatoForYtelse: LocalDate) =
+        (førsteDatoForYtelse.year - this.hentBursdagsAar().toInt()) <= 25
 
     fun hentFnrTilEktefelle(personHistorikkFraPdl: Personhistorikk?): String? {
         val fnrTilEktefelle =
@@ -24,11 +24,11 @@ object RelasjonFunksjoner {
         return fnrTilEktefelle
     }
 
-    fun hentFnrTilBarn(familierelasjoner: List<Familierelasjon>): List<String> {
+    fun hentFnrTilBarn(familierelasjoner: List<Familierelasjon>, førsteDatoForYtelse: LocalDate): List<String> {
         return familierelasjoner
             .filter { FodselsnummerValidator.isValid(it.relatertPersonsIdent) }
             .filter { it.erBarn() }
-            .filter { it.relatertPersonsIdent.erBarnUnder25Aar() }
+            .filter { it.relatertPersonsIdent.erBarnUnder25Aar(førsteDatoForYtelse) }
             .map { it.relatertPersonsIdent }
     }
 
--- src/test/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjonerTest.kt
+++ src/test/kotlin/no/nav/medlemskap/regler/funksjoner/RelasjonFunksjonerTest.kt
@@ -22,13 +22,14 @@ class RelasjonFunksjonerTest {
     @Test
     fun `hent fnr til barn`() {
         val familierelasjoner = listOf(
-            lagFamilierelasjon(relatertPersonIdent = "25079821473", minRolleForPerson = Familierelasjonsrolle.FAR),
+            lagFamilierelasjon(relatertPersonIdent = "25079528660", minRolleForPerson = Familierelasjonsrolle.FAR),
             lagFamilierelasjon(relatertPersonIdent = "26079447659", relatertPersonsRolle = Familierelasjonsrolle.MOR),
-            lagFamilierelasjon(relatertPersonIdent = "09069802246", minRolleForPerson = Familierelasjonsrolle.FAR)
+            lagFamilierelasjon(relatertPersonIdent = "09069534888", minRolleForPerson = Familierelasjonsrolle.FAR)
         )
 
-        val fnrTilBarn = RelasjonFunksjoner.hentFnrTilBarn(familierelasjoner)
-        val forventetSvar = listOf("25079821473", "09069802246")
+        val førsteDatoForYtelse = LocalDate.of(2020, 12, 12)
+        val fnrTilBarn = RelasjonFunksjoner.hentFnrTilBarn(familierelasjoner, førsteDatoForYtelse)
+        val forventetSvar = listOf("25079528660", "09069534888")
         assertEquals(2, fnrTilBarn.size)
         assertEquals(forventetSvar, fnrTilBarn)
     }
