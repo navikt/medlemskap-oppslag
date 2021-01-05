ccbf0b29749d3d6889c0532a36f2c60e85581a4b
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-01-05 15:12:28 +0100

    Fixed merge-conflicts

--- src/main/kotlin/no/nav/medlemskap/routes/CreateDatagrunnlag.kt
+++ src/main/kotlin/no/nav/medlemskap/routes/CreateDatagrunnlag.kt
@@ -7,12 +7,8 @@ import no.nav.medlemskap.common.FeatureToggles
 import no.nav.medlemskap.common.endretStatsborgerskapSisteÅretCounter
 import no.nav.medlemskap.common.flereStatsborgerskapCounter
 import no.nav.medlemskap.common.ytelseCounter
-import no.nav.medlemskap.domene.Datagrunnlag
-import no.nav.medlemskap.domene.InputPeriode
-import no.nav.medlemskap.domene.Kontrollperiode
+import no.nav.medlemskap.domene.*
 import no.nav.medlemskap.domene.Kontrollperiode.Companion.førsteDatoForYtelse
-import no.nav.medlemskap.domene.Request
-import no.nav.medlemskap.domene.Ytelse
 import no.nav.medlemskap.domene.Ytelse.Companion.name
 import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold
 import no.nav.medlemskap.domene.barn.DataOmBarn
@@ -22,7 +18,6 @@ import no.nav.medlemskap.domene.ektefelle.PersonhistorikkEktefelle
 import no.nav.medlemskap.domene.personhistorikk.Familierelasjon.Companion.hentFnrTilBarn
 import no.nav.medlemskap.domene.personhistorikk.Familierelasjon.Companion.hentFnrTilEktefelle
 import no.nav.medlemskap.domene.personhistorikk.Personhistorikk
-import no.nav.medlemskap.domene.personhistorikk.Statsborgerskap.Companion.erAnnenStatsborger
 import no.nav.medlemskap.domene.personhistorikk.Statsborgerskap.Companion.harEndretSisteÅret
 import no.nav.medlemskap.regler.funksjoner.ArbeidsforholdFunksjoner.fraOgMedDatoForArbeidsforhold
 import no.nav.medlemskap.regler.funksjoner.ArbeidsforholdFunksjoner.registrerAntallAnsatteHosJuridiskEnhet
@@ -74,8 +69,8 @@ suspend fun defaultCreateDatagrunnlag(
     val oppgaver = gosysOppgaver.await()
     val ytelse: Ytelse = finnYtelse(request.ytelse, clientId)
 
-    val oppholdstillatelse = if (FeatureToggles.FEATURE_UDI.enabled &&
-        personHistorikkFraPdl.statsborgerskap.erAnnenStatsborger(request.periode, request.førsteDagForYtelse)
+    val oppholdstillatelse = if (FeatureToggles.FEATURE_UDI.enabled //&&
+        //personHistorikkFraPdl.statsborgerskap.erAnnenStatsborger(request.periode, request.førsteDagForYtelse)
     ) {
         val oppholdsstatusRequest = async { services.udiService.hentOppholdstillatelseer(request.fnr) }
         oppholdsstatusRequest.await()
