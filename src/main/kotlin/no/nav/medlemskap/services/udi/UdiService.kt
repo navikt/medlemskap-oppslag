7d99d3748dbeb78eb813b3afc5d32a2fec7d9820
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-01-06 14:46:33 +0100

    Metrikk for arbeidsadgang fra udi

--- src/main/kotlin/no/nav/medlemskap/services/udi/UdiService.kt
+++ src/main/kotlin/no/nav/medlemskap/services/udi/UdiService.kt
@@ -1,9 +1,12 @@
 package no.nav.medlemskap.services.udi
 
 import no.nav.medlemskap.clients.udi.UdiClient
+import no.nav.medlemskap.common.arbeidsadgangtyperCounter
 
 class UdiService(private val udiClient: UdiClient) {
 
-    suspend fun hentOppholdstillatelseer(fnr: String) =
-        udiClient.hentOppholdstatusResultat(fnr = fnr)?.let { UdiMapper.mapTilOppholdstillatelser(it) }
+    suspend fun hentOppholdstillatelseer(fnr: String) {
+        val udiResultat = udiClient.hentOppholdstatusResultat(fnr = fnr)?.let { UdiMapper.mapTilOppholdstillatelser(it) }
+        arbeidsadgangtyperCounter(udiResultat?.arbeidsadgang?.arbeidsadgangType?.name).increment()
+    }
 }
