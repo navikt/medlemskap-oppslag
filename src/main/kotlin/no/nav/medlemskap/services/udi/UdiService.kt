064acbf440fba87f44f619723ee68f495d494bdd
Stian S. Douzette <stians.douzette@nav.no>
2021-01-13 09:06:12 +0100

    Legger til counter for harTillatelse, harArbeidsadgang og gjeldendeOppholdsstatus

--- src/main/kotlin/no/nav/medlemskap/services/udi/UdiService.kt
+++ src/main/kotlin/no/nav/medlemskap/services/udi/UdiService.kt
@@ -2,13 +2,21 @@ package no.nav.medlemskap.services.udi
 
 import no.nav.medlemskap.clients.udi.UdiClient
 import no.nav.medlemskap.common.arbeidsadgangtyperCounter
+import no.nav.medlemskap.common.harArbeidsadgangCounter
+import no.nav.medlemskap.common.harTillatelseCounter
+import no.nav.medlemskap.common.oppholdstillatelseTypeCounter
 import no.nav.medlemskap.domene.Oppholdstillatelse
 
 class UdiService(private val udiClient: UdiClient) {
 
     suspend fun hentOppholdstillatelseer(fnr: String): Oppholdstillatelse? {
         val udiResultat = udiClient.hentOppholdstatusResultat(fnr = fnr)?.let { UdiMapper.mapTilOppholdstillatelser(it) }
+
         arbeidsadgangtyperCounter(udiResultat?.arbeidsadgang?.arbeidsadgangType?.name).increment()
+        harArbeidsadgangCounter(udiResultat?.arbeidsadgang?.harArbeidsadgang).increment()
+        harTillatelseCounter(udiResultat?.gjeldendeOppholdsstatus?.harTillatelse).increment()
+        oppholdstillatelseTypeCounter(udiResultat?.gjeldendeOppholdsstatus)
+
         return udiResultat
     }
 }
