d4a02ee260bdb7943616a2bc523edaf686f302b5
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2021-01-29 11:55:01 +0100

    Oppgraderer core dependencies som kotlin, ktor, gradle og graphql

--- pdl-client/build.gradle.kts
+++ pdl-client/build.gradle.kts
@@ -1,4 +1,5 @@
-val graphqlKotlinClientVersion = "3.6.2"
+val graphqlKotlinClientVersion = "4.0.0-alpha.12"
+val coroutinesVersion = "1.3.9"
 
 plugins {
     kotlin("jvm")
@@ -7,11 +8,14 @@ plugins {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("com.expediagroup:graphql-kotlin-client:$graphqlKotlinClientVersion")
+    implementation("com.expediagroup:graphql-kotlin-ktor-client:$graphqlKotlinClientVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
 }
 
 tasks.withType<com.expediagroup.graphql.plugin.gradle.tasks.GraphQLGenerateClientTask> {
     packageName.set("no.nav.medlemskap.clients.pdl.generated")
     schemaFile.set(file("${project.projectDir}/src/main/resources/pdl/pdl-api-sdl.graphqls"))
     queryFileDirectory.set("${project.projectDir}/src/main/resources/pdl")
+    clientType.set(com.expediagroup.graphql.plugin.gradle.config.GraphQLClientType.KTOR)
 }
