7bbf038511084ed4222468f61f474547a4b9488e
Stian S. Douzette <stians.douzette@nav.no>
2021-02-08 05:53:53 +0100

    Regel 17 har bruker arbeidsforhold?

--- src/main/kotlin/no/nav/medlemskap/regler/v1/arbeidsforhold/HarBrukerArbeidsforholdRegel.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/v1/arbeidsforhold/HarBrukerArbeidsforholdRegel.kt
@@ -1,38 +1,34 @@
-package no.nav.medlemskap.regler.v1.frilanser
+package no.nav.medlemskap.regler.v1.arbeidsforhold
 
 import no.nav.medlemskap.domene.Datagrunnlag
 import no.nav.medlemskap.domene.InputPeriode
 import no.nav.medlemskap.domene.Ytelse
 import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold
-import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold.Companion.arbeidsforholdForYrkestype
-import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforholdstype
 import no.nav.medlemskap.regler.common.RegelId
 import no.nav.medlemskap.regler.common.Resultat
 import no.nav.medlemskap.regler.common.Resultat.Companion.ja
 import no.nav.medlemskap.regler.common.Resultat.Companion.nei
-import no.nav.medlemskap.regler.v1.arbeidsforhold.ArbeidsforholdRegel
 import java.time.LocalDate
 
-class ErArbeidsforholdetFrilanserRegel(
+class HarBrukerArbeidsforholdRegel(
     ytelse: Ytelse,
     private val periode: InputPeriode,
     private val førsteDagForYtelse: LocalDate?,
     private val arbeidsforhold: List<Arbeidsforhold>,
-    regelId: RegelId = RegelId.REGEL_17_1
+    regelId: RegelId = RegelId.REGEL_17
 ) : ArbeidsforholdRegel(regelId, ytelse, periode, førsteDagForYtelse) {
 
     override fun operasjon(): Resultat {
         return when {
-            arbeidsforhold.arbeidsforholdForYrkestype(kontrollPeriodeForArbeidsforhold)
-                .any { it == Arbeidsforholdstype.FRILANSER } -> ja(regelId)
-            else -> nei(regelId)
+            arbeidsforhold.isEmpty() -> nei(regelId)
+            else -> ja(regelId)
         }
     }
 
     companion object {
 
-        fun fraDatagrunnlag(datagrunnlag: Datagrunnlag): ErArbeidsforholdetFrilanserRegel {
-            return ErArbeidsforholdetFrilanserRegel(
+        fun fraDatagrunnlag(datagrunnlag: Datagrunnlag): HarBrukerArbeidsforholdRegel {
+            return HarBrukerArbeidsforholdRegel(
                 ytelse = datagrunnlag.ytelse,
                 periode = datagrunnlag.periode,
                 førsteDagForYtelse = datagrunnlag.førsteDagForYtelse,
---
+++ src/test/resources/dokumentasjon/features/hovedregler/arbeidsforhold/regel_17_har_bruker_arbeidsforhold.feature
@@ -0,0 +1,22 @@
+# language: no
+# encoding: UTF-8
+
+Egenskap: Regel 17: Har bruker arbeidsforhold?
+
+  Scenario: Bruker uten arbeidsforhold får "Nei"
+
+    Når regel "17" kjøres med følgende parametre
+      | Fra og med dato | Til og med dato | Har hatt arbeid utenfor Norge |
+      | 30.01.2020      | 30.01.2021      | Nei                           |
+
+    Så skal svaret være "Nei"
+
+  Scenario: Bruker med arbeidsforhold får "Ja"
+
+    Gitt følgende arbeidsforhold fra AAReg
+      | Fra og med dato | Til og med dato | Arbeidsgivertype | Arbeidsforholdstype |
+      | 01.01.2018      |                 | Organisasjon     | NORMALT             |
+
+    Når regel "17" kjøres med følgende parametre
+      | Fra og med dato | Til og med dato | Har hatt arbeid utenfor Norge |
+      | 30.01.2020      | 30.01.2021      | Nei                           |
