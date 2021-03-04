1f40fc5c8a4d4bab0e064d4047030cbf609d3108
Per Birger Eriksen <per.birger.eriksen@nav.no>
2021-03-04 15:27:20 +0100

    Tok vekk beskrivelse fra Årsak

--- src/main/kotlin/no/nav/medlemskap/regler/common/Årsak.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/common/Årsak.kt
@@ -1,13 +1,7 @@
 package no.nav.medlemskap.regler.common
 
 data class Årsak(val regelId: RegelId, val avklaring: String, val svar: Svar) {
-    val beskrivelse = beskrivelse()
     val begrunnelse = regelId.begrunnelse(svar)
-
-    fun beskrivelse(): String {
-        val regelIdStr = "Regel ${regelId.identifikator}: "
-        return "$regelIdStr $avklaring $svar"
-    }
 
     companion object {
         fun fraResultat(resultat: Resultat): Årsak {
