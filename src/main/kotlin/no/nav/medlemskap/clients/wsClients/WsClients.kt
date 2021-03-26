af6f68046e057d36bcc00a6534bb6ea2cac0d136
Silje Risvik <silje.risvik@nav.no>
2021-03-26 10:53:10 +0100

    slettet duplikat for wsClients

--- src/main/kotlin/no/nav/medlemskap/clients/wsClients/WsClients.kt
+++ src/main/kotlin/no/nav/medlemskap/clients/wsClients/WsClients.kt
@@ -1,7 +1,11 @@
-package no.nav.medlemskap.clients.wsClients
+package no.nav.medlemskap.services
 
+import io.github.resilience4j.retry.Retry
+import mu.KotlinLogging
 import no.nav.medlemskap.clients.CallIdInterceptor
 import no.nav.medlemskap.clients.sts.configureFor
+import no.nav.medlemskap.clients.udi.UdiClient
+import no.nav.medlemskap.clients.udi.UdiFactory
 import no.nav.medlemskap.common.MetricFeature
 import org.apache.cxf.ext.logging.LoggingFeature
 import org.apache.cxf.ws.addressing.WSAddressingFeature
@@ -11,12 +15,14 @@ class WsClients(
     private val stsClientWs: STSClient,
     private val callIdGenerator: () -> String
 ) {
-
-    private val features get() = listOf(WSAddressingFeature(), LoggingFeature(), MetricFeature())
+    private val wsAddressingFeature = WSAddressingFeature()
+    private val features = listOf(wsAddressingFeature, LoggingFeature(), MetricFeature())
     private val outInterceptors get() = listOf(CallIdInterceptor(callIdGenerator))
+    private val logger = KotlinLogging.logger { }
 
     companion object {
         init {
+
             System.setProperty("javax.xml.soap.SAAJMetaFactory", "com.sun.xml.messaging.saaj.soap.SAAJMetaFactoryImpl")
         }
     }
@@ -25,4 +31,14 @@ class WsClients(
         apply {
             stsClientWs.configureFor(this)
         }
+
+    fun oppholdstillatelse(endpointUrl: String, retry: Retry?) : UdiClient {
+       logger.info{"wsAddressingFeature isEnabled" + wsAddressingFeature.isEnabled}
+       logger.info { "features.get(0).isEnabled" + features[0].isEnabled }
+       logger.info { "features.get(0) " + features[0] }
+       return UdiFactory.create(endpointUrl, features, outInterceptors)
+            .withSts().let { port ->
+                UdiClient(port, retry)
+            }
+    }
 }
