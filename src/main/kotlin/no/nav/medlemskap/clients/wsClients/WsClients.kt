9f2c673224330c76a28d24a917a4bf9bdff3fd66
Silje Risvik <silje.risvik@nav.no>
2021-03-26 12:10:48 +0100

    Fjerner alt av logging

--- src/main/kotlin/no/nav/medlemskap/clients/wsClients/WsClients.kt
+++ src/main/kotlin/no/nav/medlemskap/clients/wsClients/WsClients.kt
@@ -8,18 +8,15 @@ import no.nav.medlemskap.clients.udi.UdiClient
 import no.nav.medlemskap.clients.udi.UdiFactory
 import no.nav.medlemskap.common.MetricFeature
 import org.apache.cxf.ext.logging.LoggingFeature
-import org.apache.cxf.feature.AbstractFeature
-import org.apache.cxf.ws.addressing.WSAddressingFeature
 import org.apache.cxf.ws.security.trust.STSClient
 
 class WsClients(
     private val stsClientWs: STSClient,
     private val callIdGenerator: () -> String
 ) {
-    private val wsAddressingFeature = createWsAdressingFeature()
+
     private val features = listOf(LoggingFeature(), MetricFeature())
     private val outInterceptors get() = listOf(CallIdInterceptor(callIdGenerator))
-    private val logger = KotlinLogging.logger { }
 
     companion object {
         init {
@@ -34,21 +31,11 @@ class WsClients(
 
     fun oppholdstillatelse(endpointUrl: String, retry: Retry?): UdiClient {
 
-        logger.info { "wsAddressingFeature isEnabled " + wsAddressingFeature.isEnabled }
-        logger.info { "wsAddressingFeature isAddressingRequired " + wsAddressingFeature.isAddressingRequired }
-        logger.info { "wsAddressingFeature allowDuplicates " + wsAddressingFeature.isAllowDuplicates }
-        logger.info { "wsAddressingFeature responses " + wsAddressingFeature.responses }
-
         return UdiFactory.create(endpointUrl, features, outInterceptors)
             .withSts().let { port ->
                 UdiClient(port, retry)
             }
     }
 
-    private fun createWsAdressingFeature(): WSAddressingFeature {
-        val wsAddressingFeature = WSAddressingFeature()
-        wsAddressingFeature.isAddressingRequired = true
-        return wsAddressingFeature
-    }
 }
 
