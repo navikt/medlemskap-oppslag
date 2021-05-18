e170e10d8a61780278ca48cf52c6bab23fe0ca30
StianSDouzette <59877023+StianSDouzette@users.noreply.github.com>
2021-05-18 08:06:51 +0200

    Regel 22 har utenlandsopphold (#500)
    * utenlandsopphold WIP
    
    * Regel 22: har bruker utenlandsopphold i arbeidsforhold
    
    Co-authored-by: Stian S. Douzette <stians.douzette@nav.no>

--- src/main/kotlin/no/nav/medlemskap/common/exceptions/GradertAdresseException.kt
+++ src/main/kotlin/no/nav/medlemskap/common/exceptions/GradertAdresseException.kt
@@ -1,6 +1,5 @@
 package no.nav.medlemskap.common.exceptions
 
 import no.nav.medlemskap.domene.Ytelse
-import java.lang.Exception
 
 class GradertAdresseException(val system: String, val ytelse: Ytelse) : Exception()
