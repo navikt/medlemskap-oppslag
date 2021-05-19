20473b4b1e3c4a13f073199f8fdf1fa5e107b6ee
hans-jacob Melby <hans-jacob.melby@nav.no>
2021-05-19 11:34:14 +0200

    lagt til regel i regelflyt lagt til test av regelflyt

--- src/main/kotlin/no/nav/medlemskap/regler/v1/arbeidsforhold/HarBrukerSammenhengendeArbeidsforholdRegel.kt
+++ src/main/kotlin/no/nav/medlemskap/regler/v1/arbeidsforhold/HarBrukerSammenhengendeArbeidsforholdRegel.kt
@@ -4,7 +4,6 @@ import no.nav.medlemskap.domene.Datagrunnlag
 import no.nav.medlemskap.domene.Ytelse
 import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold
 import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold.Companion.erSammenhengendeIKontrollPeriode
-import no.nav.medlemskap.domene.personhistorikk.Statsborgerskap
 import no.nav.medlemskap.regler.common.RegelId
 import no.nav.medlemskap.regler.common.Resultat
 import no.nav.medlemskap.regler.common.Resultat.Companion.ja
@@ -15,7 +14,7 @@ class HarBrukerSammenhengendeArbeidsforholdRegel(
     ytelse: Ytelse,
     private val startDatoForYtelse: LocalDate,
     private val arbeidsforhold: List<Arbeidsforhold>,
-    private val statsborgerskap: List<Statsborgerskap>,
+    // private val statsborgerskap: List<Statsborgerskap>,
     regelId: RegelId = RegelId.REGEL_3
 ) : ArbeidsforholdRegel(regelId, ytelse, startDatoForYtelse) {
 
@@ -34,7 +33,7 @@ class HarBrukerSammenhengendeArbeidsforholdRegel(
                 ytelse = datagrunnlag.ytelse,
                 startDatoForYtelse = datagrunnlag.startDatoForYtelse,
                 arbeidsforhold = datagrunnlag.arbeidsforhold,
-                statsborgerskap = datagrunnlag.pdlpersonhistorikk.statsborgerskap
+                // statsborgerskap = datagrunnlag.pdlpersonhistorikk.statsborgerskap
             )
         }
     }
