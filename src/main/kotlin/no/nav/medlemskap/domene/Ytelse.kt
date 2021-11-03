dd481eabee1719a0bd5085d02562cae548f78bcb
hans-jacob Melby <hans-jacob.melby@nav.no>
2021-11-03 08:55:53 +0100

    lagt in klientid for medlemskap-oppslag-prod

--- src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
@@ -22,7 +22,8 @@ enum class Ytelse {
             "496b0ded-cfdb-4430-b7b6-b568504de005" to LOVME, // dev
             "2719da58-489e-4185-9ee6-74b7e93763d2" to LOVME_GCP, // dev
             "d24ecfa9-ffa7-48dc-a9f3-f8ae3138e603" to LOVME_FUNKSJONELLE_TESTER, // dev
-            "ee472fd1-3621-4600-a6ac-69d3662e993f" to SYKEPENGER //sykepenger-lytter-dev
+            "ee472fd1-3621-4600-a6ac-69d3662e993f" to SYKEPENGER, //sykepenger-lytter-dev
+            "23600ac9-019c-445d-87a4-2df4996e6f63" to LOVME_GCP //Prod verifisering
         )
 
         fun fromClientId(clientId: String?): Ytelse? = clientIdToYtelseMap[clientId]
