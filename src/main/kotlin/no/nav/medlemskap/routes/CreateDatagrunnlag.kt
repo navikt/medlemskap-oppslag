a875d401ceedc84e0fd7908abc9ff5ae9e99ff00
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-01-14 20:17:38 +0100

    CreateDatagrunnlag.kt kaller alle tjenester igjen

--- src/main/kotlin/no/nav/medlemskap/routes/CreateDatagrunnlag.kt
+++ src/main/kotlin/no/nav/medlemskap/routes/CreateDatagrunnlag.kt
@@ -3,7 +3,11 @@ package no.nav.medlemskap.routes
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 import mu.KotlinLogging
+import net.logstash.logback.argument.StructuredArguments.kv
 import no.nav.medlemskap.clients.Services
+import no.nav.medlemskap.clients.udi.UdiRequest
+import no.nav.medlemskap.common.FeatureToggles
+import no.nav.medlemskap.common.objectMapper
 import no.nav.medlemskap.common.ytelseCounter
 import no.nav.medlemskap.domene.Datagrunnlag
 import no.nav.medlemskap.domene.Kontrollperiode.Companion.startDatoForYtelse
@@ -11,8 +15,10 @@ import no.nav.medlemskap.domene.Request
 import no.nav.medlemskap.domene.Ytelse
 import no.nav.medlemskap.domene.Ytelse.Companion.name
 import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold.Companion.fraOgMedDatoForArbeidsforhold
-import no.nav.medlemskap.domene.personhistorikk.Personhistorikk
+import no.nav.medlemskap.domene.personhistorikk.ForelderBarnRelasjon
+import no.nav.medlemskap.domene.personhistorikk.Statsborgerskap.Companion.erAnnenStatsborger
 import no.nav.medlemskap.services.FamilieService
+import v1.mt_1067_nav.no.udi.HentPersonstatusFault
 
 private val secureLogger = KotlinLogging.logger("tjenestekall")
 
@@ -34,16 +40,13 @@ suspend fun defaultCreateDatagrunnlag(
         )
     }
 
-    val aktorIder = null // = services.pdlService.hentAlleAktorIder(request.fnr, callId)
+    val aktorIder = services.pdlService.hentAlleAktorIder(request.fnr, callId)
     val medlemskapsunntakRequest = async { services.medlService.hentMedlemskapsunntak(request.fnr, callId) }
-    // dette kallet øker med 4 tråder for hver request
     val journalPosterRequest = async { services.safService.hentJournaldata(request.fnr, callId) }
-    val gosysOppgaver = null // = async { services.oppgaveService.hentOppgaver(aktorIder, callId) }
+    val gosysOppgaver = async { services.oppgaveService.hentOppgaver(aktorIder, callId) }
 
-    val personHistorikk = null // services.pdlService.hentPersonHistorikkTilBruker(request.fnr, callId)
+    val personHistorikk = services.pdlService.hentPersonHistorikkTilBruker(request.fnr, callId)
 
-    val dataOmBrukersBarn = null
-    /*
     val dataOmBrukersBarn = familieService.hentDataOmBarn(
         ForelderBarnRelasjon.hentFnrTilBarn(
             personHistorikk.forelderBarnRelasjon,
@@ -52,27 +55,19 @@ suspend fun defaultCreateDatagrunnlag(
         callId
     )
 
-     */
-
-    val dataOmEktefelle = null
-    /*
-
     val dataOmEktefelle = familieService.hentDataOmEktefelle(
         fnrTilEktefelle = ForelderBarnRelasjon.hentFnrTilEktefelle(personHistorikk),
         periode = request.periode,
         startDatoForYtelse = startDatoForYtelse,
         callId = callId
     )
-    */
 
     val medlemskap = medlemskapsunntakRequest.await()
     val arbeidsforhold = arbeidsforholdRequest.await()
     val journalPoster = journalPosterRequest.await()
-    // val oppgaver = gosysOppgaver.await()
+    val oppgaver = gosysOppgaver.await()
     val ytelse: Ytelse = finnYtelse(request.ytelse, clientId)
 
-    val oppholdstillatelse = null
-    /*
     val oppholdstillatelse = if (FeatureToggles.FEATURE_UDI.enabled &&
         personHistorikk.statsborgerskap.erAnnenStatsborger(startDatoForYtelse)
     ) {
@@ -112,7 +107,6 @@ suspend fun defaultCreateDatagrunnlag(
     } else {
         null
     }
-    */
 
     ytelseCounter(ytelse.name()).increment()
 
@@ -121,13 +115,10 @@ suspend fun defaultCreateDatagrunnlag(
         periode = request.periode,
         førsteDagForYtelse = request.førsteDagForYtelse,
         brukerinput = request.brukerinput,
-        pdlpersonhistorikk = Personhistorikk(
-            emptyList(), emptyList(), emptyList(), emptyList(), emptyList(), emptyList(),
-            emptyList()
-        ), // personHistorikk,
+        pdlpersonhistorikk = personHistorikk,
         medlemskap = medlemskap,
         arbeidsforhold = arbeidsforhold,
-        oppgaver = emptyList(),
+        oppgaver = oppgaver,
         dokument = journalPoster,
         ytelse = ytelse,
         dataOmBarn = dataOmBrukersBarn,
