146c2420c7a4219431b3aaac100d2ae2c631ef0f
Stian S. Douzette <stians.douzette@nav.no>
2022-03-09 11:07:28 +0100

    Oppdaterer testrammeverk med ny soknadIkkeAvgjort endring

--- src/test/kotlin/no/nav/medlemskap/cucumber/steps/udi/OppholdstillatelsePåSammeVilkårBuilder.kt
+++ src/test/kotlin/no/nav/medlemskap/cucumber/steps/udi/OppholdstillatelsePåSammeVilkårBuilder.kt
@@ -3,13 +3,14 @@ package no.nav.medlemskap.cucumber.steps.udi
 import no.nav.medlemskap.domene.OppholdstillaelsePaSammeVilkarType
 import no.nav.medlemskap.domene.OppholdstillatelsePaSammeVilkar
 import no.nav.medlemskap.domene.Periode
+import no.nav.medlemskap.domene.SoknadIkkeAvgjort
 
 class OppholdstillatelsePåSammeVilkårBuilder {
     var periodeBuilder = PeriodeBuilder()
     var periode: Periode? = periodeBuilder.build()
     var type: OppholdstillaelsePaSammeVilkarType? = OppholdstillaelsePaSammeVilkarType.PERMANENT
     var harTillatelse: Boolean? = true
-    var soknadIkkeAvgjort: Boolean = true
+    var soknadIkkeAvgjort: SoknadIkkeAvgjort = SoknadIkkeAvgjort(type, periode)
 
     fun build(): OppholdstillatelsePaSammeVilkar {
         return OppholdstillatelsePaSammeVilkar(
