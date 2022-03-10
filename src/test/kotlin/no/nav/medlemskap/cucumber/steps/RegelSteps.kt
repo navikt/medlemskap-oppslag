23428f6d9343689d63b7d6c4ccf28dd58479a205
Stian S. Douzette <stians.douzette@nav.no>
2022-03-10 07:46:12 +0100

    fjerner println

--- src/test/kotlin/no/nav/medlemskap/cucumber/steps/RegelSteps.kt
+++ src/test/kotlin/no/nav/medlemskap/cucumber/steps/RegelSteps.kt
@@ -298,8 +298,6 @@ class RegelSteps : No {
 
             val jsonResponse = medlemskapRequest(medlemskapsparametre)
 
-            println(jsonResponse)
-
             resultat = Response.fraJson(jsonResponse).resultat
 
             JSONAssert.assertEquals(
