f8784ba5104383f59e9785d05180081bf98fee81
hans-jacob Melby <hans-jacob.melby@nav.no>
2023-01-26 14:32:41 +0100

    lagt til medlemskap-dagpengelytter i prod som godkjent klient

--- src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Ytelse.kt
@@ -15,8 +15,8 @@ enum class Ytelse {
             "a16f1075-4482-4577-baab-d2a7323655fa" to SYKEPENGER, // Prod (skal utgå) - sparkel-medlemskap
             "2a9e52c1-a4f5-41cf-a5ce-b9ec9e83b588" to SYKEPENGER, // Prod:tbd - sparkel-medlemskap
             "4487496e-9ba3-4c3a-a083-f45ca3878775" to SYKEPENGER, // dev
-            "337777ed-eced-4765-9504-c6e7aebec9e5" to DAGPENGER, // Prod - dp-oppslag-medlemskap
-            "d13c07e8-5159-4647-a9c1-0790d5bc9af9" to DAGPENGER, // dev
+            "bd721a1b-6ee9-4836-ae29-c0a3b6825e61" to DAGPENGER, // Prod - medlemskap-dagpengelytter
+            "d13c07e8-5159-4647-a9c1-0790d5bc9af9" to DAGPENGER, // Dev - medlemskap-dagpengelytter
             "4de2bae4-f86a-4c12-aeef-74177c6e724a" to ENSLIG_FORSORGER, // Prod - familie-ef-soknad-api
             "ea03f30a-413b-4f0e-bfae-477aad57de67" to ENSLIG_FORSORGER, // dev
             "496b0ded-cfdb-4430-b7b6-b568504de005" to LOVME, // dev
