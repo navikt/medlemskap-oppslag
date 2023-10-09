0f90b601d11fd73671f696ecaa23547c262f6890
Stian S. Douzette <stians.douzette@nav.no>
2023-10-09 09:45:07 +0200

    fjerner unødvendig komma

--- src/main/kotlin/no/nav/medlemskap/routes/EvalueringRoute.kt
+++ src/main/kotlin/no/nav/medlemskap/routes/EvalueringRoute.kt
@@ -308,7 +308,7 @@ private fun loggResponse(fnr: String, response: Response, endpoint: String = "/"
             kv("har_utflytting", response.datagrunnlag.pdlpersonhistorikk.harUtflyttingFraNorge()),
             kv("yrkeskoder", response.datagrunnlag.arbeidsforhold.alleAktiveYrkeskoderDerTomErNull()),
             kv("har_medlperiode_uten_arbeidsforhold", response.datagrunnlag.harPeriodeUtenMedlemskapOgIkkeArbeidsforhold()),
-            kv("endpoint", endpoint),
+            kv("endpoint", endpoint)
             //kv("regleroverstyrt", response.resultat.erReglerOverstyrt())
         )
     }.onFailure {
