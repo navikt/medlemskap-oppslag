712e8aa0896aaed969b09d5f72147d74f0288dda
hans-jacob Melby <hans-jacob.melby@nav.no>
2023-10-19 13:26:12 +0200

    lagt til mulighet for nye brukerinput (brukerspørsmål) til regel motoren

--- src/main/kotlin/no/nav/medlemskap/domene/Request.kt
+++ src/main/kotlin/no/nav/medlemskap/domene/Request.kt
@@ -14,7 +14,51 @@ data class Request(
 )
 
 data class Brukerinput(
-    val arbeidUtenforNorge: Boolean
+    val arbeidUtenforNorge: Boolean,
+    val oppholdstilatelse: Oppholdstilatelse? = null,
+    val utfortAarbeidUtenforNorge: UtfortAarbeidUtenforNorge? = null,
+    val oppholdUtenforEos: OppholdUtenforEos? = null,
+    val oppholdUtenforNorge: OppholdUtenforNorge? = null
+)
+data class Oppholdstilatelse(
+    val id: String,
+    val sporsmalstekst: String?,
+    val svar: Boolean,
+    val vedtaksdato: LocalDate,
+    val vedtaksTypePermanent: Boolean,
+    val perioder: List<Periode> = mutableListOf()
+)
+data class UtfortAarbeidUtenforNorge(
+    val id: String,
+    val sporsmalstekst: String?,
+    val svar: Boolean,
+    val arbeidUtenforNorge: List<ArbeidUtenforNorge>
+)
+data class ArbeidUtenforNorge(
+    val id: String,
+    val arbeidsgiver: String,
+    val land: String,
+    val perioder: List<Periode>
+)
+data class OppholdUtenforNorge(
+    val id: String,
+    val sporsmalstekst: String?,
+    val svar: Boolean,
+    val oppholdUtenforNorge: List<Opphold>
+)
+
+data class OppholdUtenforEos(
+    val id: String,
+    val sporsmalstekst: String?,
+    val svar: Boolean,
+    val oppholdUtenforEOS: List<Opphold>
+)
+
+data class Opphold(
+    val id: String,
+    val land: String,
+    val grunn: String,
+    val perioder: List<Periode>
 )
 
 data class InputPeriode(
