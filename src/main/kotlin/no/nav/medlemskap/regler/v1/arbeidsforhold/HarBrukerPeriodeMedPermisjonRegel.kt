fa724a2cd48ca5ce6526302fa0263797439da159
Stian S. Douzette <stians.douzette@nav.no>
2024-10-09 11:02:03 +0200

    Grunnlag for regel 32

---
+++ src/main/kotlin/no/nav/medlemskap/regler/v1/arbeidsforhold/HarBrukerPeriodeMedPermisjonRegel.kt
@@ -0,0 +1,36 @@
+package no.nav.medlemskap.regler.v1.arbeidsforhold
+
+import no.nav.medlemskap.domene.Datagrunnlag
+import no.nav.medlemskap.domene.Ytelse
+import no.nav.medlemskap.domene.arbeidsforhold.Arbeidsforhold
+import no.nav.medlemskap.regler.common.RegelId
+import no.nav.medlemskap.regler.common.Resultat
+import no.nav.medlemskap.regler.common.Resultat.Companion.ja
+import no.nav.medlemskap.regler.common.Resultat.Companion.nei
+import java.time.LocalDate
+
+class HarBrukerPeriodeMedPermisjonRegel(
+    ytelse: Ytelse,
+    startDatoForYtelse: LocalDate,
+    private val arbeidsforhold: List<Arbeidsforhold>,
+    regelId: RegelId = RegelId.REGEL_32
+) : ArbeidsforholdRegel(regelId, ytelse, startDatoForYtelse) {
+
+    override fun operasjon(): Resultat {
+        return when {
+            arbeidsforhold -> nei(regelId)
+            else -> ja(regelId)
+        }
+    }
+
+    companion object {
+
+        fun fraDatagrunnlag(datagrunnlag: Datagrunnlag): HarBrukerPeriodeMedPermisjonRegel {
+            return HarBrukerPeriodeMedPermisjonRegel(
+                ytelse = datagrunnlag.ytelse,
+                startDatoForYtelse = datagrunnlag.startDatoForYtelse,
+                arbeidsforhold = datagrunnlag.arbeidsforhold
+            )
+        }
+    }
+}
